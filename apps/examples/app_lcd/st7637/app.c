0cfedd45bda52a193e3425ace1c5f134b43bd6ad
tk <tk@midibox.org>
2008-10-04 00:46:12 +0000

    programming example for ST7637 driver

---
+++ apps/examples/app_lcd/st7637/app.c
@@ -0,0 +1,219 @@
+// $Id$
+/*
+ * Demo application for ST7637 GLCD, stuffed on a STM32 Primer
+ *
+ * ==========================================================================
+ *
+ *  Copyright (C) 2008 Thorsten Klose (tk@midibox.org)
+ *  Licensed for personal non-commercial use only.
+ *  All other rights reserved.
+ * 
+ * ==========================================================================
+ */
+
+/////////////////////////////////////////////////////////////////////////////
+// Include files
+/////////////////////////////////////////////////////////////////////////////
+
+#include <mios32.h>
+#include "app.h"
+
+#include <glcd_font.h>
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called after startup to initialize the application
+/////////////////////////////////////////////////////////////////////////////
+void APP_Init(void)
+{
+  MIOS32_BOARD_LED_Init(0xffffffff); // initialize all LEDs
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This task is running endless in background
+/////////////////////////////////////////////////////////////////////////////
+void APP_Background(void)
+{
+  // print static screen
+  MIOS32_LCD_FontInit(GLCD_FONT_NORMAL);
+  MIOS32_LCD_BColourSet(0x00, 0x00, 0x00);
+  MIOS32_LCD_FColourSet(0xff, 0xff, 0xff);
+
+  // clear LCD
+  MIOS32_LCD_Clear();
+
+  // print text
+  MIOS32_LCD_CursorSet(3, 3);
+  printf("ST7637 LCD");
+
+  MIOS32_LCD_CursorSet(7, 5);
+  printf("powered by");
+
+  // endless loop: print animations
+  s8 mios_r = 0;
+  s8 mios_g = 0;
+  s8 mios_b = 0;
+  s8 dir = 1;
+  s8 knob_icon_ctr[4] = {0, 3, 6, 9}; // memo: 12 icons
+  s8 knob_icon_delay_ctr[4] = {0, 2, 4, 6};
+  const s8 knob_icon_x[4] = {0, 100, 0, 100}; // memo: icon width 28
+  const s8 knob_icon_y[4] = {0, 0, 104, 104}; // memo: icon height 32
+
+  s8 vmeter_icon_ctr[2] = {0, 5}; // memo: 28 icons (14 used)
+  s8 vmeter_icon_dir[2] = {1, 1};
+  s8 vmeter_icon_delay_ctr[2] = {1, 4};
+  const s8 vmeter_icon_x[2] = {0, 120}; // memo: icon width 8
+  const s8 vmeter_icon_y[2] = {48, 48}; // memo: icon height 32
+
+  s8 hmeter_icon_ctr[2] = {6, 11}; // memo: 28 icons (14 used)
+  s8 hmeter_icon_dir[2] = {1, 0};
+  s8 hmeter_icon_delay_ctr[2] = {4, 2};
+  const s8 hmeter_icon_x[2] = {50, 50}; // memo: icon width 28
+  const s8 hmeter_icon_y[2] = {0, 120}; // memo: icon height 8
+
+  while( 1 ) {
+    s32 i;
+
+    // toggle the state of all LEDs (allows to measure the execution speed with a scope)
+    MIOS32_BOARD_LED_Set(0xffffffff, ~MIOS32_BOARD_LED_Get());
+
+    // colour-cycle "MIOS32" up and down :-)
+    // ST7637 supports 5bit r, 6bit g and 5bit b
+    if( dir ) {
+      if( mios_r < 0x1f )
+	++mios_r;
+      else if( mios_g < 0x3f )
+	++mios_g;
+      else if( mios_b < 0x1f )
+	++mios_b;
+      else
+	dir = 0;
+    } else {
+      if( mios_r > 0x00 )
+	--mios_r;
+      else if( mios_g > 0x00 )
+	--mios_g;
+      else if( mios_b > 0x00 )
+	--mios_b;
+      else
+	dir = 1;
+    }
+
+    // set new colour
+    MIOS32_LCD_FColourSet(mios_r, mios_g, mios_b);
+
+    // print "MIOS32"
+    MIOS32_LCD_FontInit(GLCD_FONT_BIG);
+    MIOS32_LCD_GCursorSet(16, 52);
+    printf("MIOS32");
+
+    // icons with different colour
+    MIOS32_LCD_FColourSet(dir ? mios_r : ~mios_r, ~mios_g, dir ? mios_b : ~mios_b);
+
+    // print turning Knob icons at all edges
+    MIOS32_LCD_FontInit(GLCD_FONT_KNOB_ICONS); // memo: 12 icons, icon size: 28x24
+    for(i=0; i<4; ++i) {
+      if( ++knob_icon_delay_ctr[i] > 10 ) {
+	knob_icon_delay_ctr[i] = 0;
+	if( ++knob_icon_ctr[i] >= 12 )
+	  knob_icon_ctr[i] = 0;
+      }
+      MIOS32_LCD_GCursorSet(knob_icon_x[i], knob_icon_y[i]);
+      MIOS32_LCD_PrintChar(knob_icon_ctr[i]);
+    }
+
+    // print vmeter icons
+    MIOS32_LCD_FontInit(GLCD_FONT_METER_ICONS_V); // memo: 28 icons, 14 used, icon size: 8x32
+    for(i=0; i<2; ++i) {
+      if( ++vmeter_icon_delay_ctr[i] > 5 ) {
+	vmeter_icon_delay_ctr[i] = 0;
+	if( vmeter_icon_dir[i] ) {
+	  if( ++vmeter_icon_ctr[i] >= 13 )
+	    vmeter_icon_dir[i] = 0;
+	} else {
+	  if( --vmeter_icon_ctr[i] < 1 )
+	    vmeter_icon_dir[i] = 1;
+	}
+      }
+      MIOS32_LCD_GCursorSet(vmeter_icon_x[i], vmeter_icon_y[i]);
+      MIOS32_LCD_PrintChar(vmeter_icon_ctr[i]);
+    }
+
+    // print hmeter icons
+    MIOS32_LCD_FontInit(GLCD_FONT_METER_ICONS_H); // memo: 28 icons, 14 used, icon size: 28x8
+    for(i=0; i<2; ++i) {
+      if( ++hmeter_icon_delay_ctr[i] > 7 ) {
+	hmeter_icon_delay_ctr[i] = 0;
+	if( hmeter_icon_dir[i] ) {
+	  if( ++hmeter_icon_ctr[i] >= 13 )
+	    hmeter_icon_dir[i] = 0;
+	} else {
+	  if( --hmeter_icon_ctr[i] < 1 )
+	    hmeter_icon_dir[i] = 1;
+	}
+      }
+      MIOS32_LCD_GCursorSet(hmeter_icon_x[i], hmeter_icon_y[i]);
+      MIOS32_LCD_PrintChar(hmeter_icon_ctr[i]);
+    }
+  }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//  This hook is called when a complete MIDI event has been received
+/////////////////////////////////////////////////////////////////////////////
+void APP_NotifyReceivedEvent(u8 port, mios32_midi_package_t midi_package)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when a SysEx byte has been received
+/////////////////////////////////////////////////////////////////////////////
+void APP_NotifyReceivedSysEx(u8 port, u8 sysex_byte)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called before the shift register chain is scanned
+/////////////////////////////////////////////////////////////////////////////
+void APP_SRIO_ServicePrepare(void)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called after the shift register chain has been scanned
+/////////////////////////////////////////////////////////////////////////////
+void APP_SRIO_ServiceFinish(void)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when a button has been toggled
+// pin_value is 1 when button released, and 0 when button pressed
+/////////////////////////////////////////////////////////////////////////////
+void APP_DIN_NotifyToggle(u32 pin, u32 pin_value)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when an encoder has been moved
+// incrementer is positive when encoder has been turned clockwise, else
+// it is negative
+/////////////////////////////////////////////////////////////////////////////
+void APP_ENC_NotifyChange(u32 encoder, s32 incrementer)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when a pot has been moved
+/////////////////////////////////////////////////////////////////////////////
+void APP_AIN_NotifyChange(u32 pin, u32 pin_value)
+{
+}
