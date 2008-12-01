c97d111857226daf6cebc2697c6d64686644906d
tk <tk@midibox.org>
2008-12-01 22:42:06 +0000

    re-arranged source files for better transparancy

--- apps/sequencers/midibox_seq_v4/core/app.c
+++ apps/sequencers/midibox_seq_v4/core/app.c
@@ -1,6 +1,6 @@
 // $Id$
 /*
- * Sequencer Demo
+ * MIDIbox SEQ V4
  *
  * ==========================================================================
  *
@@ -16,12 +16,12 @@
 /////////////////////////////////////////////////////////////////////////////
 
 #include <mios32.h>
-
-#if DEFAULT_SRM_ENABLED
 #include <blm8x8.h>
-#endif
 
-#include "seq_demo.h"
+#include "tasks.h"
+
+#include "app.h"
+
 #include "seq_core.h"
 #include "seq_par.h"
 #include "seq_trg.h"
@@ -29,30 +29,52 @@
 
 
 /////////////////////////////////////////////////////////////////////////////
-// Local types
+// Encoder Configuration
 /////////////////////////////////////////////////////////////////////////////
+#define NUM_ENCODERS 17
+const mios32_enc_config_t encoders[NUM_ENCODERS] = {
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 6, .cfg.pos=2 }, // Data Wheel
+
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 1, .cfg.pos=6 }, // V-Pot 1
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 1, .cfg.pos=4 }, // V-Pot 2
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 1, .cfg.pos=2 }, // V-Pot 3
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 1, .cfg.pos=0 }, // V-Pot 4
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 3, .cfg.pos=6 }, // V-Pot 5
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 3, .cfg.pos=4 }, // V-Pot 6
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 3, .cfg.pos=2 }, // V-Pot 7
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 3, .cfg.pos=0 }, // V-Pot 8
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 4, .cfg.pos=6 }, // V-Pot 9
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 4, .cfg.pos=4 }, // V-Pot 10
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 4, .cfg.pos=2 }, // V-Pot 11
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 4, .cfg.pos=0 }, // V-Pot 12
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 5, .cfg.pos=6 }, // V-Pot 13
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 5, .cfg.pos=4 }, // V-Pot 14
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 5, .cfg.pos=2 }, // V-Pot 15
+  { .cfg.type=DETENTED2, .cfg.speed=NORMAL, .cfg.speed_par=0, .cfg.sr= 5, .cfg.pos=0 }, // V-Pot 16
+};
 
 
 /////////////////////////////////////////////////////////////////////////////
-// Local prototypes
+// This hook is called after startup to initialize the application
 /////////////////////////////////////////////////////////////////////////////
-
-
-/////////////////////////////////////////////////////////////////////////////
-// Global variables
-/////////////////////////////////////////////////////////////////////////////
-
-
-/////////////////////////////////////////////////////////////////////////////
-// Local variables
-/////////////////////////////////////////////////////////////////////////////
-
-
-/////////////////////////////////////////////////////////////////////////////
-// Initialisation
-/////////////////////////////////////////////////////////////////////////////
-s32 Init(u32 mode)
+void APP_Init(void)
 {
+  s32 i;
+
+  // initialize all LEDs
+  MIOS32_BOARD_LED_Init(0xffffffff);
+
+#if 1
+  // initialize encoders
+  for(i=0; i<NUM_ENCODERS; ++i)
+    MIOS32_ENC_ConfigSet(i, encoders[i]);
+#endif
+
+  // initialize second CLCD
+  MIOS32_LCD_DeviceSet(1);
+  MIOS32_LCD_Init(0);
+  MIOS32_LCD_DeviceSet(0);
+
 #if DEFAULT_SRM_ENABLED
   // init BLM
   BLM8X8_Init(0);
@@ -69,15 +91,33 @@ s32 Init(u32 mode)
   SEQ_LED_Init(0);
   SEQ_UI_Init(0);
 
-  return 0; // no error
+  // start tasks (differs between MIOS32 and MacOS)
+  TASKS_Init(0);
 }
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This task is running endless in background
+/////////////////////////////////////////////////////////////////////////////
+void APP_Background(void)
+{
+  // toggle the state of all LEDs (allows to measure the execution speed with a scope)
+  MIOS32_BOARD_LED_Set(0xffffffff, ~MIOS32_BOARD_LED_Get());
+
+  // call LCD Handler
+  SEQ_UI_LCD_Handler();
+
+  // update LEDs
+  SEQ_UI_LED_Handler();
+}
+
 
 /////////////////////////////////////////////////////////////////////////////
 //  This hook is called when a complete MIDI event has been received
 /////////////////////////////////////////////////////////////////////////////
 void APP_NotifyReceivedEvent(u8 port, mios32_midi_package_t midi_package)
 {
-  MIOS32_MIDI_SendPackage(port, midi_package);
+  //  MIOS32_MIDI_SendPackage(port, midi_package);
 }
 
 
@@ -86,6 +126,38 @@ void APP_NotifyReceivedEvent(u8 port, mios32_midi_package_t midi_package)
 /////////////////////////////////////////////////////////////////////////////
 void APP_NotifyReceivedSysEx(u8 port, u8 sysex_byte)
 {
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when a byte has been received via COM interface
+/////////////////////////////////////////////////////////////////////////////
+void APP_NotifyReceivedCOM(mios32_com_port_t port, u8 byte)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called before the shift register chain is scanned
+/////////////////////////////////////////////////////////////////////////////
+void APP_SRIO_ServicePrepare(void)
+{
+#if DEFAULT_SRM_ENABLED
+  // prepare DOUT registers of 8x8 BLM to drive the column
+  BLM8X8_PrepareCol();
+#endif
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called after the shift register chain has been scanned
+/////////////////////////////////////////////////////////////////////////////
+void APP_SRIO_ServiceFinish(void)
+{
+#if DEFAULT_SRM_ENABLED
+  // call the BLM8X8_GetRow function after scan is finished to capture the read DIN values
+  BLM8X8_GetRow();
+#endif
 }
 
 
@@ -142,44 +214,11 @@ void APP_ENC_NotifyChange(u32 encoder, s32 incrementer)
 
 
 /////////////////////////////////////////////////////////////////////////////
-// This hook is called before the shift register chain is scanned
+// This hook is called when a pot has been moved
 /////////////////////////////////////////////////////////////////////////////
-void APP_SRIO_ServicePrepare(void)
+void APP_AIN_NotifyChange(u32 pin, u32 pin_value)
 {
-#if DEFAULT_SRM_ENABLED
-  // prepare DOUT registers of 8x8 BLM to drive the column
-  BLM8X8_PrepareCol();
-#endif
 }
-
-
-/////////////////////////////////////////////////////////////////////////////
-// This hook is called after the shift register chain has been scanned
-/////////////////////////////////////////////////////////////////////////////
-void APP_SRIO_ServiceFinish(void)
-{
-#if DEFAULT_SRM_ENABLED
-  // call the BLM8X8_GetRow function after scan is finished to capture the read DIN values
-  BLM8X8_GetRow();
-#endif
-}
-
-
-/////////////////////////////////////////////////////////////////////////////
-// This task is running endless in background
-/////////////////////////////////////////////////////////////////////////////
-void APP_Background(void)
-{
-  // toggle the state of all LEDs (allows to measure the execution speed with a scope)
-  MIOS32_BOARD_LED_Set(0xffffffff, ~MIOS32_BOARD_LED_Get());
-
-  // call LCD Handler
-  SEQ_UI_LCD_Handler();
-
-  // update LEDs
-  SEQ_UI_LED_Handler();
-}
-
 
 /////////////////////////////////////////////////////////////////////////////
 // This task is called periodically each mS
@@ -194,3 +233,4 @@ void SEQ_TASK_Period1mS(void)
   BLM8X8_ButtonHandler(APP_BLM8X8_NotifyToggle);
 #endif
 }
+
