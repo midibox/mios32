90e3bc8bd9872726a7cbf40ef77eeba483209647
tk <tk@midibox.org>
2008-12-07 22:19:56 +0000

    implementation of track direction modes

---
+++ apps/sequencers/midibox_seq_v4/core/seq_random.c
@@ -0,0 +1,69 @@
+// $Id$
+/*
+ * Random generator
+ *
+ * ==========================================================================
+ *
+ *  Copyright (C) 2008 Thorsten Klose (tk@midibox.org)
+ *  Licensed for personal non-commercial use only.
+ *  All other rights reserved.
+ * 
+ * ==========================================================================
+ */
+
+/////////////////////////////////////////////////////////////////////////////
+// Include files
+/////////////////////////////////////////////////////////////////////////////
+
+#include <mios32.h>
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Local variables
+/////////////////////////////////////////////////////////////////////////////
+
+// initial seed
+static unsigned int random_value = 0xdeadbabe;
+
+
+/////////////////////////////////////////////////////////////////////////////
+// random generator function
+/////////////////////////////////////////////////////////////////////////////
+u32 SEQ_RANDOM_Gen(u32 seed)
+{
+  if( seed )
+    random_value = seed;
+
+  // MEMO: for thread safeness, we would have to pass a pointer to random_value through the SEQ_RANDOM_Gen() function
+  // however, multiple threads just increase the randomness even more - therefore this approach is fine ;-)
+
+  // TODO: combine with timer values for even more randomness
+  rand_r(&random_value);
+
+  return random_value;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// returns scaled random number
+/////////////////////////////////////////////////////////////////////////////
+u32 SEQ_RANDOM_Gen_Range(u32 min, u32 max)
+{
+  // values equal? -> no random number required
+  if( min == max )
+    return min;
+
+  // swap min/max if reversed
+  if( min > max ) {
+    u32 tmp;
+    tmp = min;
+    min = max;
+    max = tmp;
+  }
+
+  // generate new random number
+  rand_r(&random_value);
+
+  // return result within the given range
+  return random_value % (max-min+1);
+}
