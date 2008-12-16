710088064d89b28cc636744707d40904c16097be
tk <tk@midibox.org>
2008-12-16 23:53:15 +0000

    ongoing rename

--- modules/sequencer/seq_midi.c
+++ modules/sequencer/seq_midi.c
@@ -17,7 +17,7 @@
 
 #include <mios32.h>
 
-#include "seq_midi.h"
+#include "seq_midi_out.h"
 #include "seq_bpm.h"
 
 
@@ -26,13 +26,13 @@
 /////////////////////////////////////////////////////////////////////////////
 
 // an item of the MIDI output queue
-typedef struct seq_midi_queue_item_t {
+typedef struct seq_midi_out_queue_item_t {
   u8                    port;
   u8                    event_type;
   mios32_midi_package_t package;
   u32                   timestamp;
-  struct seq_midi_queue_item_t *next;
-} seq_midi_queue_item_t;
+  struct seq_midi_out_queue_item_t *next;
+} seq_midi_out_queue_item_t;
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -44,22 +44,22 @@ typedef struct seq_midi_queue_item_t {
 // Global variables
 /////////////////////////////////////////////////////////////////////////////
 
-u32 seq_midi_queue_size; // for analysis purposes
+u32 seq_midi_out_queue_size; // for analysis purposes
 
 
 /////////////////////////////////////////////////////////////////////////////
 // Local variables
 /////////////////////////////////////////////////////////////////////////////
 
-static seq_midi_queue_item_t *midi_queue;
+static seq_midi_out_queue_item_t *midi_queue;
 
 /////////////////////////////////////////////////////////////////////////////
 // Initialisation
 /////////////////////////////////////////////////////////////////////////////
-s32 SEQ_MIDI_Init(u32 mode)
+s32 SEQ_MIDI_OUT_Init(u32 mode)
 {
   midi_queue = NULL;
-  seq_midi_queue_size = 0;
+  seq_midi_out_queue_size = 0;
 
   return 0; // no error
 }
@@ -69,14 +69,14 @@ s32 SEQ_MIDI_Init(u32 mode)
 // This function sends a MIDI event over given port at a given time
 // returns -1 if out of memory
 /////////////////////////////////////////////////////////////////////////////
-s32 SEQ_MIDI_Send(mios32_midi_port_t port, mios32_midi_package_t midi_package, seq_midi_event_type_t event_type, u32 timestamp)
+s32 SEQ_MIDI_OUT_Send(mios32_midi_port_t port, mios32_midi_package_t midi_package, seq_midi_out_event_type_t event_type, u32 timestamp)
 {
   // create new item
-  seq_midi_queue_item_t *new_item;
-  if( (new_item=(seq_midi_queue_item_t *)pvPortMalloc(sizeof(seq_midi_queue_item_t))) == NULL ) {
+  seq_midi_out_queue_item_t *new_item;
+  if( (new_item=(seq_midi_out_queue_item_t *)pvPortMalloc(sizeof(seq_midi_out_queue_item_t))) == NULL ) {
     return -1; // allocation error
   } else {
-    ++seq_midi_queue_size;
+    ++seq_midi_out_queue_size;
 
     new_item->port = port;
     new_item->package = midi_package;
@@ -86,23 +86,23 @@ s32 SEQ_MIDI_Send(mios32_midi_port_t port, mios32_midi_package_t midi_package, s
   }
  
 #if 0
-  printf("[SEQ_MIDI_Send:%d] %02x %02x %02x\n\r", timestamp, midi_package.evnt0, midi_package.evnt1, midi_package.evnt2);
+  printf("[SEQ_MIDI_OUT_Send:%d] %02x %02x %02x\n\r", timestamp, midi_package.evnt0, midi_package.evnt1, midi_package.evnt2);
 #endif
 
   // search in queue for last item which has the same (or earlier) timestamp
-  seq_midi_queue_item_t *item;
+  seq_midi_out_queue_item_t *item;
   if( (item=midi_queue) == NULL ) {
     // no item in queue -- first element
     midi_queue = new_item;
   } else {
     u8 insert_before_item = 0;
-    seq_midi_queue_item_t *last_item = midi_queue;
-    seq_midi_queue_item_t *next_item;
+    seq_midi_out_queue_item_t *last_item = midi_queue;
+    seq_midi_out_queue_item_t *next_item;
     do {
       // Clock and Tempo events are sorted before CC and Note events at a given timestamp
-      if( (event_type == SEQ_MIDI_ClkEvent || event_type == SEQ_MIDI_TempoEvent ) && 
+      if( (event_type == SEQ_MIDI_OUT_ClkEvent || event_type == SEQ_MIDI_OUT_TempoEvent ) && 
 	  item->timestamp == timestamp &&
-	  (item->event_type == SEQ_MIDI_OnEvent || item->event_type == SEQ_MIDI_CCEvent) ) {
+	  (item->event_type == SEQ_MIDI_OUT_OnEvent || item->event_type == SEQ_MIDI_OUT_CCEvent) ) {
 	// found any event with same timestamp, insert clock before these events
 	// note that the Clock event order doesn't get lost if clock events 
 	// are queued at the same timestamp (e.g. MIDI start -> MIDI clock)
@@ -113,9 +113,9 @@ s32 SEQ_MIDI_Send(mios32_midi_port_t port, mios32_midi_package_t midi_package, s
       // CCs are sorted before notes at a given timestamp
       // (new CC before On events at the same timestamp)
       // CCs are still played after Off or Clock events
-      if( event_type == SEQ_MIDI_CCEvent && 
+      if( event_type == SEQ_MIDI_OUT_CCEvent && 
 	  item->timestamp == timestamp &&
-	  item->event_type == SEQ_MIDI_OnEvent ) {
+	  item->event_type == SEQ_MIDI_OUT_OnEvent ) {
 	// found On event with same timestamp, play CC before On event
 	insert_before_item = 1;
 	break;
@@ -156,16 +156,16 @@ s32 SEQ_MIDI_Send(mios32_midi_port_t port, mios32_midi_package_t midi_package, s
 /////////////////////////////////////////////////////////////////////////////
 // This function empties the queue and plays all off events
 /////////////////////////////////////////////////////////////////////////////
-s32 SEQ_MIDI_FlushQueue(void)
+s32 SEQ_MIDI_OUT_FlushQueue(void)
 {
-  seq_midi_queue_item_t *item;
+  seq_midi_out_queue_item_t *item;
   while( (item=midi_queue) != NULL ) {
-    if( item->event_type == SEQ_MIDI_OffEvent )
+    if( item->event_type == SEQ_MIDI_OUT_OffEvent )
       MIOS32_MIDI_SendPackage(item->port, item->package);
 
     midi_queue = item->next;
     vPortFree(item);
-    --seq_midi_queue_size;
+    --seq_midi_out_queue_size;
   }
 
   return 0; // no error
@@ -176,20 +176,20 @@ s32 SEQ_MIDI_FlushQueue(void)
 // This function should be called periodically (1 mS) to check timestamped
 // MIDI events which have to be sent
 /////////////////////////////////////////////////////////////////////////////
-s32 SEQ_MIDI_Handler(void)
+s32 SEQ_MIDI_OUT_Handler(void)
 {
   // search in queue for items which have to be played now (or have been missed earlier)
   // note that we are going through a sorted list, therefore we can exit once a timestamp
   // has been found which has to be played later than now
 
-  seq_midi_queue_item_t *item;
+  seq_midi_out_queue_item_t *item;
   while( (item=midi_queue) != NULL && item->timestamp <= SEQ_BPM_TickGet() ) {
 #if 0
-    printf("[SEQ_MIDI_Handler:%d] %02x %02x %02x\n\r", item->timestamp, item->package.evnt0, item->package.evnt1, item->package.evnt2);
+    printf("[SEQ_MIDI_OUT_Handler:%d] %02x %02x %02x\n\r", item->timestamp, item->package.evnt0, item->package.evnt1, item->package.evnt2);
 #endif
 
     // if tempo event: change BPM stored in midi_package.ALL
-    if( item->event_type == SEQ_MIDI_TempoEvent ) {
+    if( item->event_type == SEQ_MIDI_OUT_TempoEvent ) {
       SEQ_BPM_Set(item->package.ALL);
     } else {
       MIOS32_MIDI_SendPackage(item->port, item->package);
@@ -197,7 +197,7 @@ s32 SEQ_MIDI_Handler(void)
 
     midi_queue = item->next;
     vPortFree(item);
-    --seq_midi_queue_size;
+    --seq_midi_out_queue_size;
   }
 
   return 0; // no error
--- modules/sequencer/seq_midi.h
+++ modules/sequencer/seq_midi.h
@@ -1,6 +1,6 @@
 // $Id$
 /*
- * Header file for MIDI routines
+ * Header file for MIDI output routines
  *
  * ==========================================================================
  *
@@ -11,8 +11,8 @@
  * ==========================================================================
  */
 
-#ifndef _SEQ_MIDI_H
-#define _SEQ_MIDI_H
+#ifndef _SEQ_MIDI_OUT_H
+#define _SEQ_MIDI_OUT_H
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -25,29 +25,29 @@
 /////////////////////////////////////////////////////////////////////////////
 
 typedef enum {
-  SEQ_MIDI_ClkEvent,   // always sent first before any other timestamped event
-  SEQ_MIDI_TempoEvent, // dito - changes the BPM rate (located in midi_package.ALL)
-  SEQ_MIDI_CCEvent,    // sent before notes
-  SEQ_MIDI_OnEvent,    // note on event
-  SEQ_MIDI_OffEvent    // note off event - sent by SEQ_MIDI_FlushQueue when queue is emptied (e.g. on Stop/Pause)
-} seq_midi_event_type_t;
+  SEQ_MIDI_OUT_ClkEvent,   // always sent first before any other timestamped event
+  SEQ_MIDI_OUT_TempoEvent, // dito - changes the BPM rate (located in midi_package.ALL)
+  SEQ_MIDI_OUT_CCEvent,    // sent before notes
+  SEQ_MIDI_OUT_OnEvent,    // note on event
+  SEQ_MIDI_OUT_OffEvent    // note off event - sent by SEQ_MIDI_OUT_FlushQueue when queue is emptied (e.g. on Stop/Pause)
+} seq_midi_out_event_type_t;
 
 
 /////////////////////////////////////////////////////////////////////////////
 // Prototypes
 /////////////////////////////////////////////////////////////////////////////
 
-extern s32 SEQ_MIDI_Init(u32 mode);
+extern s32 SEQ_MIDI_OUT_Init(u32 mode);
 
-extern s32 SEQ_MIDI_Send(mios32_midi_port_t port, mios32_midi_package_t midi_package, seq_midi_event_type_t event_type, u32 timestamp);
-extern s32 SEQ_MIDI_FlushQueue(void);
-extern s32 SEQ_MIDI_Handler(void);
+extern s32 SEQ_MIDI_OUT_Send(mios32_midi_port_t port, mios32_midi_package_t midi_package, seq_midi_out_event_type_t event_type, u32 timestamp);
+extern s32 SEQ_MIDI_OUT_FlushQueue(void);
+extern s32 SEQ_MIDI_OUT_Handler(void);
 
 
 /////////////////////////////////////////////////////////////////////////////
 // Export global variables
 /////////////////////////////////////////////////////////////////////////////
 
-extern u32 seq_midi_queue_size;
+extern u32 seq_midi_out_queue_size;
 
-#endif /* _SEQ_MIDI_H */
+#endif /* _SEQ_MIDI_OUT_H */
