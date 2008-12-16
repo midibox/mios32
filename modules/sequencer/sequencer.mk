5fbe6822ac55a87c046fbb957f799bfb1064a70a
tk <tk@midibox.org>
2008-12-16 23:53:15 +0000

    ongoing rename

--- modules/sequencer/sequencer.mk
+++ modules/sequencer/sequencer.mk
@@ -7,8 +7,8 @@ C_INCLUDE += -I $(MIOS32_PATH)/modules/sequencer
 
 # add modules to thumb sources (TODO: provide makefile option to add code to ARM sources)
 THUMB_SOURCE += \
-	$(MIOS32_PATH)/modules/midifile/seq_bpm.c \
-	$(MIOS32_PATH)/modules/midifile/seq_midi.c
+	$(MIOS32_PATH)/modules/sequencer/seq_bpm.c \
+	$(MIOS32_PATH)/modules/sequencer/seq_midi_out.c
 
 
 # directories and files that should be part of the distribution (release) package
