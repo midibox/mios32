9b19cf1faa3ca14ba17465beee1a6b21800e151e
tk <tk@midibox.org>
2008-12-31 18:37:52 +0000

    BSL now integrated into application binary

---
+++ bootloader/gen_inc_file.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+##################################################################################################
+# $Id$
+# This perl script generates an include file which contains the BSL code
+# Usage:   perl gen_inc_file.pl <bin-file> <inc-file> <array-name>
+# Example: perl gen_inc_file.pl project.bin mios32_bsl_test.inc mios32_bsl_test_image
+##################################################################################################
+
+use Fcntl;
+
+if( scalar(@ARGV) != 3 ) {
+  die "Usage:   perl gen_inc_file.pl <bin-file> <inc-file> <array-name>\n" .
+      "Example: perl gen_inc_file.pl project.bin mios32_bsl_test.inc mios32_bsl_test_code\n";
+}
+
+my ($bin_file, $inc_file, $array_name) = @ARGV;
+my $array_declaration = "__attribute__ ((section(\".mios32_bsl\"))) const u8";
+my $dump_size = 0x4000; # reserved for BSL and EEPROM emulation
+
+# read .bin file into $content array
+open(IN, "<${bin_file}") || die "ERROR: '${bin_file}' does not exist!\n";
+print "Reading '${bin_file}'...\n";
+my $len;
+my $content;
+if( ($len=sysread(IN, $content, $dump_size)) <= 0 ) {
+  die "ERROR: Files longer than ${dump_size} are not supported (Length: ${len} bytes)\n";
+}
+close(IN);
+
+# convert to C array
+print "Writing out to '${inc_file}'\n";
+open(OUT, ">${inc_file}") || die "ERROR: cannot open '${inc_file}'!\n";
+
+printf OUT "// \$Id: \$\n";
+printf OUT "// generated with '$0 " . join(" ", @ARGV) . "'\n\n";
+printf OUT "${array_declaration} ${array_name}[${dump_size}] = {\n", $len;
+my $line = "";
+for($i=0; $i<$dump_size; ++$i) {
+  my $b = ($i >= $len) ? 0xff : ord(substr($content, $i, 1));
+  $line .= sprintf("0x%02x,", $b);
+  if( ($i % 16) == 15 ) {
+    print OUT "$line\n";
+    $line = "";
+  }
+}
+if( length($line) ) {
+  print OUT "$line\n";
+}
+printf OUT "};\n";
+
+close(OUT);
--- bootloader/main.c
+++ bootloader/main.c
@@ -2,6 +2,12 @@
 /*
  * MIOS32 Bootloader
  *
+ * TODO: provide SysEx command which allows that BSL stays in wait loop
+ * TODO: don't re-init USB anymore
+ * TODO: MIOS32 should directly branch to BSL, no USB reset
+ * TODO: "hold pin" to select USB after power-on reset and to avoid branch
+ *       to application (e.g. BOOT1 could be used)
+ *
  * ==========================================================================
  *
  *  Copyright (C) 2008 Thorsten Klose (tk@midibox.org)
@@ -23,8 +29,8 @@
 // Local definitions
 /////////////////////////////////////////////////////////////////////////////
 
-// number of LED used by BSL to notify status (counting from 0)
-#define BSL_LED_NUM 0 // green LED
+// LEDs used by BSL to notify the status
+#define BSL_LED_MASK 0x00000001 // green LED
 
 
 // timer used to measure delays (TIM1..TIM8)
@@ -47,7 +53,7 @@ int main(void)
   ///////////////////////////////////////////////////////////////////////////
   MIOS32_SYS_Init(0);
 
-  MIOS32_BOARD_LED_Init(1 << BSL_LED_NUM);
+  MIOS32_BOARD_LED_Init(BSL_LED_MASK);
 
 
   ///////////////////////////////////////////////////////////////////////////
@@ -63,7 +69,7 @@ int main(void)
 
   // time base configuration
   TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-  TIM_TimeBaseStructure.TIM_Period = 50000; // waiting for 5 seconds (-> 50000 * 100 uS)
+  TIM_TimeBaseStructure.TIM_Period = 20000; // waiting for 2 seconds (-> 20000 * 100 uS)
   TIM_TimeBaseStructure.TIM_Prescaler = 7200-1; // for 100 uS accuracy @ 72 MHz
   TIM_TimeBaseStructure.TIM_ClockDivision = 0;
   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
@@ -79,9 +85,7 @@ int main(void)
   ///////////////////////////////////////////////////////////////////////////
   // initialize remaining peripherals
   ///////////////////////////////////////////////////////////////////////////
-  MIOS32_USB_Init(0);
-  MIOS32_UART_Init(0);
-  MIOS32_MIDI_Init(0);
+  MIOS32_MIDI_Init(0); // will also initialize UART and USB
 
 
   ///////////////////////////////////////////////////////////////////////////
@@ -101,8 +105,18 @@ int main(void)
   // clear timer pending flag and start wait loop
   TIM_ClearITPendingBit(BSL_DELAY_TIMER, TIM_IT_Update);
   do {
-    // first LED flashes in intervals for 1s with 4:1 duty cycle
-    MIOS32_BOARD_LED_Set(1 << BSL_LED_NUM, ((BSL_DELAY_TIMER->CNT % 10000) < 7500) ? (1 << BSL_LED_NUM) : 0);
+    // This is a simple way to pulsate a LED via PWM
+    // A timer in incrementer mode is used as reference, the counter value is incremented each 100 uS
+    // Within the given PWM period, we define a duty cycle based on the current counter value
+    // We periodically sweep the PWM duty cycle 100 steps up, and 100 steps down
+    u32 cnt = BSL_DELAY_TIMER->CNT;  // the reference counter (incremented each 100 uS)
+    const u32 pwm_period = 50;       // *100 uS -> 5 mS
+    const u32 pwm_sweep_steps = 100; // * 5 mS -> 500 mS
+    u32 pwm_duty = ((cnt / pwm_period) % pwm_sweep_steps) / (pwm_sweep_steps/pwm_period);
+    if( (cnt % (2*pwm_period*pwm_sweep_steps)) > pwm_period*pwm_sweep_steps )
+      pwm_duty = pwm_period-pwm_duty; // negative direction each 50*100 ticks
+    u32 led_on = ((cnt % pwm_period) > pwm_duty) ? 1 : 0;
+    MIOS32_BOARD_LED_Set(BSL_LED_MASK, led_on ? BSL_LED_MASK : 0);
 
     // handle USB messages
     MIOS32_USB_MIDI_Handler();
@@ -112,26 +126,30 @@ int main(void)
     // directly by MIOS32 to enhance command set
     MIOS32_MIDI_Receive_Handler(NULL, NULL);
 
-#if 0
-    // another request after 5 seconds
-    static int onlyonce = 0;
-    if( !onlyonce && TIM_GetITStatus(BSL_DELAY_TIMER, TIM_IT_Update) != RESET ) {
-      onlyonce=1;
-      BSL_SYSEX_SendUploadReq(UART0);    
-      BSL_SYSEX_SendUploadReq(USB0);
-    }
-#endif
-
-  } while( TIM_GetITStatus(BSL_DELAY_TIMER, TIM_IT_Update) == RESET || 1 );
-
-  // turn on LED permanently 
-  MIOS32_BOARD_LED_Set(1 << BSL_LED_NUM, 1 << BSL_LED_NUM);
+  } while( TIM_GetITStatus(BSL_DELAY_TIMER, TIM_IT_Update) == RESET );
 
   // ensure that flash write access is locked
   FLASH_Lock();
 
-  // TODO: here we will branch to the application later
-  while( 1 );
+  // branch to application if reset vector is valid (should be inside flash range)
+  u32 *reset_vector = (u32 *)0x08004004;
+  if( (*reset_vector >> 24) == 0x08 ) {
+    // reset all peripherals
+    RCC_APB2PeriphResetCmd(0xffffffff, ENABLE);
+    RCC_APB1PeriphResetCmd(0xffffffff, ENABLE);
+    RCC_APB2PeriphResetCmd(0xffffffff, DISABLE);
+    RCC_APB1PeriphResetCmd(0xffffffff, DISABLE);
+
+    // branch to application
+    void (*application)(void) = (void *)*reset_vector;
+    application();
+  }
+
+  // otherwise flash LED fast (BSL failed to start application)
+  while( 1 ) {
+    u32 led_on = ((BSL_DELAY_TIMER->CNT % 1000) > 500) ? 1 : 0;
+    MIOS32_BOARD_LED_Set(BSL_LED_MASK, led_on ? BSL_LED_MASK : 0);
+  }
 
   return 0; // will never be reached
 }
