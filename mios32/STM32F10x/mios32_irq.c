380d63f9281560a9ffa399fe814e54b6dac3ae51
tk <tk@midibox.org>
2009-02-17 23:22:17 +0000

    MIOS32_IRQ_* routines take previous priority level into account

--- mios32/STM32F10x/mios32_irq.c
+++ mios32/STM32F10x/mios32_irq.c
@@ -27,6 +27,9 @@
 // nested functions disable them
 static u32 nested_ctr;
 
+// stored priority level before IRQ has been disabled (important for co-existence with vPortEnterCritical)
+static u32 prev_primask;
+
 
 /////////////////////////////////////////////////////////////////////////////
 //! This function disables all interrupts (nested)
@@ -34,9 +37,18 @@ static u32 nested_ctr;
 /////////////////////////////////////////////////////////////////////////////
 s32 MIOS32_IRQ_Disable(void)
 {
+  // get current priority if nested level == 0
+  if( !nested_ctr ) {
+    __asm volatile (			   \
+		    "	mrs %0, primask\n" \
+		    : "=r" (prev_primask)  \
+		    );
+  }
+
+  // disable interrupts
   __asm volatile ( \
-		  "	mov r0, #1	\n" \
-		  "	msr primask, r0	\n" \
+		  "	mov r0, #1     \n" \
+		  "	msr primask, r0\n" \
 		  :::"r0"	 \
 		  );
 
@@ -49,16 +61,22 @@ s32 MIOS32_IRQ_Disable(void)
 /////////////////////////////////////////////////////////////////////////////
 //! This function enables all interrupts (nested)
 //! \return < 0 on errors
+//! \return -1 on nesting errors (MIOS32_IRQ_Disable() hasn't been called before)
 /////////////////////////////////////////////////////////////////////////////
 s32 MIOS32_IRQ_Enable(void)
 {
+  // check for nesting error
+  if( nested_ctr == 0 )
+    return -1; // nesting error
+
+  // decrease nesting level
   --nested_ctr;
 
+  // set back previous priority once nested level reached 0 again
   if( nested_ctr == 0 ) {
     __asm volatile ( \
-		    "	mov r0, #0	\n" \
-		    "	msr primask, r0	\n" \
-		    :::"r0"		    \
+		    "	msr primask, %0\n" \
+		    :: "r" (prev_primask)  \
 		    );
   }
 
