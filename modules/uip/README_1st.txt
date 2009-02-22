786c920bdb7febbd22df82f3ff8834a550e5b1a4
tk <tk@midibox.org>
2009-02-22 23:02:23 +0000

    added uIP + mios32 layer to repository

---
+++ modules/uip/README_1st.txt
@@ -0,0 +1,24 @@
+$Id$
+
+uIP has been developed by Adam Dunkels, and is provided as freeware.
+
+See README for details.
+
+This package has been downloaded from:
+   http://www.sics.se/~adam/uip/index.php/Main_Page
+
+Version uip-1-0 is used.
+
+
+Following files have been added:
+  - uip.mk: has to be included into Makefile to add uIP sources
+  - mios32/clock-arch*: clock function for MIOS32 projects
+  - mios32/network-device*: access layer to MIOS32_ENC28J60 routines
+
+
+MIOS32 based usage examples can be found under
+   $MIOS32_PATH/apps/examples/ethernet
+
+
+Modifications:
+  - uip/uip-neighbor.c: commented out "printf"
---
+++ modules/uip/mios32/network-device.h
@@ -0,0 +1,24 @@
+// $Id$
+/*
+ * Header file for access functions to network device
+ *
+ * ==========================================================================
+ *
+ *  Copyright (C) 2009 Thorsten Klose (tk@midibox.org)
+ *  Licensed for personal non-commercial use only.
+ *  All other rights reserved.
+ * 
+ * ==========================================================================
+ */
+
+
+#ifndef __NETWORK_DEVICE_H__
+#define __NETWORK_DEVICE_H__
+
+extern void network_device_init(void);
+extern void network_device_check(void);
+extern int network_device_available(void);
+extern int network_device_read(void);
+extern void network_device_send(void);
+
+#endif /* __NETWORK_DEVICE_H__ */
