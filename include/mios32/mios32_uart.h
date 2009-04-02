db6cfbaed4fa5e6cc1b4ac834593d3bc61971972
tk <tk@midibox.org>
2009-04-02 22:18:06 +0000

    added option for MIDI Tx pin OD mode (enabled by default), recompiled BSL

--- include/mios32/mios32_uart.h
+++ include/mios32/mios32_uart.h
@@ -43,9 +43,19 @@
 #define MIOS32_UART0_BAUDRATE 31250
 #endif
 
+// should UART0 Tx pin configured for open drain (default) or push-pull mode?
+#ifndef MIOS32_UART0_TX_OD
+#define MIOS32_UART0_TX_OD 1
+#endif
+
 // Baudrate of UART second interface
 #ifndef MIOS32_UART1_BAUDRATE
 #define MIOS32_UART1_BAUDRATE 31250
+#endif
+
+// should UART1 Tx pin configured for open drain (default) or push-pull mode?
+#ifndef MIOS32_UART1_TX_OD
+#define MIOS32_UART1_TX_OD 1
 #endif
 
 // Interface assignment: 0 = disabled, 1 = MIDI, 2 = COM
@@ -60,12 +70,12 @@
 
 // Interface assignment: 0 = disabled, 1 = MIDI, 2 = COM
 #ifndef MIOS32_UART2_ASSIGNMENT
-#define MIOS32_UART2_ASSIGNMENT 1
+#define MIOS32_UART2_ASSIGNMENT 0
 #endif
 
 // Interface assignment: 0 = disabled, 1 = MIDI, 2 = COM
 #ifndef MIOS32_UART3_ASSIGNMENT
-#define MIOS32_UART3_ASSIGNMENT 1
+#define MIOS32_UART3_ASSIGNMENT 0
 #endif
 
 
