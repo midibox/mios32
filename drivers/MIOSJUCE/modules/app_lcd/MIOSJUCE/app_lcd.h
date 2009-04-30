a55e9663f13c3f02374f6632825fd230c6cbd207
stryd_one <stryd.one@gmail.com>
2009-04-30 02:01:13 +0000

    MIOSJUCE Rearranged. C++ sources pending.

---
+++ drivers/MIOSJUCE/modules/app_lcd/MIOSJUCE/app_lcd.h
@@ -0,0 +1,50 @@
+// $Id: app_lcd.h 95 2008-10-19 14:13:14Z tk $
+/*
+ * Header file for application specific LCD Driver
+ *
+ * ==========================================================================
+ *
+ *  Copyright (C) 2008 Thorsten Klose (tk@midibox.org)
+ *  Licensed for personal non-commercial use only.
+ *  All other rights reserved.
+ * 
+ * ==========================================================================
+ */
+
+#ifndef _APP_LCD_H
+#define _APP_LCD_H
+
+/////////////////////////////////////////////////////////////////////////////
+// Global definitions
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Global Types
+/////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Prototypes
+/////////////////////////////////////////////////////////////////////////////
+
+// hooks to MIOS32_LCD
+extern s32 APP_LCD_Init(u32 mode);
+extern s32 APP_LCD_Data(u8 data);
+extern s32 APP_LCD_Cmd(u8 cmd);
+extern s32 APP_LCD_Clear(void);
+extern s32 APP_LCD_CursorSet(u16 column, u16 line);
+extern s32 APP_LCD_GCursorSet(u16 x, u16 y);
+extern s32 APP_LCD_PrintChar(char c);
+extern s32 APP_LCD_SpecialCharInit(u8 num, u8 table[8]);
+extern s32 APP_LCD_BColourSet(u8 r, u8 g, u8 b);
+extern s32 APP_LCD_FColourSet(u8 r, u8 g, u8 b);
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Export global variables
+/////////////////////////////////////////////////////////////////////////////
+
+
+#endif /* _APP_LCD_H */
