8bfe80b53607ba9e0cb423b35183b4ce9e2bfe11
stryd_one <stryd.one@gmail.com>
2009-04-30 01:54:09 +0000

    MIOSJUCE Rearranged. C++ sources pending.

---
+++ programming_models/MIOSJUCE/Main.cpp
@@ -0,0 +1,157 @@
+/*
+  ==============================================================================
+
+   MIOS32 Emulation with JUCE
+   Copyright 2009 stryd_one
+
+  ==============================================================================
+*/
+
+// This file lets us set up any special config that we need for this app..
+#include "juce_AppConfig.h"
+
+// And this includes all the juce headers..
+#include <juce.h>
+
+// this is so that the global MIDI settings can be exported to the wrapper
+#include <JUCEmidi.hpp>
+
+// this is for the global dialogs like MIDI IO and About for the wrapper
+#include "MIOSJUCEDialogs.h"
+
+// one for the main component. The app can include the rest.
+#include "MainComponent.h"
+
+//==============================================================================
+/** 
+    This is the top-level window that we'll pop up. Inside it, we'll create and
+    show a component from the MainComponent.cpp file (you can open this file using
+    the Jucer to edit it).
+*/
+class MIOSJUCEWindow : public DocumentWindow
+{
+public:
+    //==============================================================================
+    MIOSJUCEWindow() 
+        : DocumentWindow (T("MIOSJUCE"),
+                          Colours::lightgrey, 
+                          DocumentWindow::allButtons,
+                          true)
+    {
+        
+        // create the MIDI Device Manager
+        midiDeviceManager = new AudioDeviceManager();
+        
+        // Create an instance of our main content component, and add it 
+        // to our window.
+        MainComponent* const contentComponent = new MainComponent();
+        setContentComponent (contentComponent, true, true);
+        centreWithSize (getWidth(), getHeight());
+        
+        // setup the window
+        MenuItems* MIOSJUCEMenu= new MenuItems();
+        setMenuBar(MIOSJUCEMenu);
+        setUsingNativeTitleBar(true);
+        setVisible (true);
+        setResizable(true, false);
+        setResizeLimits(250,200,8192,8192);
+        
+    }
+
+    ~MIOSJUCEWindow()
+    {
+        // (the content component will be deleted automatically, so no need to do it here)
+    }
+
+    //==============================================================================
+    void closeButtonPressed()
+    {
+        // When the user presses the close button, we'll tell the app to quit. This 
+        // window will be deleted by our MIOSJUCEApplication::shutdown() method
+        // 
+        JUCEApplication::quit();
+    }
+};
+
+//==============================================================================
+/** This is the application object that is started up when Juce starts. It handles
+    the initialisation and shutdown of the whole application.
+*/
+class MIOSJUCEApplication : public JUCEApplication
+{
+    /* Important! NEVER embed objects directly inside your JUCEApplication class! Use
+       ONLY pointers to objects, which you should create during the initialise() method
+       (NOT in the constructor!) and delete in the shutdown() method (NOT in the
+       destructor!)
+
+       This is because the application object gets created before Juce has been properly
+       initialised, so any embedded objects would also get constructed too soon.
+   */
+    MIOSJUCEWindow* mIOSJUCEWindow;
+
+public:
+    //==============================================================================
+    MIOSJUCEApplication()
+        : mIOSJUCEWindow (0)
+    {
+        // NEVER do anything in here that could involve any Juce function being called
+        // - leave all your startup tasks until the initialise() method.
+    }
+
+    ~MIOSJUCEApplication()
+    {
+        // Your shutdown() method should already have done all the things necessary to
+        // clean up this app object, so you should never need to put anything in
+        // the destructor.
+
+        // Making any Juce calls in here could be very dangerous...
+    }
+
+    //==============================================================================
+    void initialise (const String& commandLine)
+    {
+        // just create the main window...
+        mIOSJUCEWindow = new MIOSJUCEWindow();
+
+        /*  ..and now return, which will fall into to the main event
+            dispatch loop, and this will run until something calls
+            JUCEAppliction::quit().
+
+            In this case, JUCEAppliction::quit() will be called by the
+            window being clicked.
+        */
+    }
+
+    void shutdown()
+    {
+        // clear up..
+
+        if (mIOSJUCEWindow != 0)
+            delete mIOSJUCEWindow;
+    }
+
+    //==============================================================================
+    const String getApplicationName()
+    {
+        return T("MIOSJUCE MIOS32 Programming Model");
+    }
+
+    const String getApplicationVersion()
+    {
+        return T("0.1");
+    }
+
+    bool moreThanOneInstanceAllowed()
+    {
+        return true;
+    }
+
+    void anotherInstanceStarted (const String& commandLine)
+    {
+    }
+};
+
+
+//==============================================================================
+// This macro creates the application's main() function..
+START_JUCE_APPLICATION (MIOSJUCEApplication)
