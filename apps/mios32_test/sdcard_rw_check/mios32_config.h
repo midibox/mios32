4e0b8c7f968a340f0ac2e3f9cbb76b6e563dcbbb
this <thismaechler@gmail.com>
2009-05-24 20:15:34 +0000

    new directory mios32_test for low-level testing apps. moved /troubleshooting/sdcard_check to here

---
+++ apps/mios32_test/sdcard_rw_check/mios32_config.h
@@ -0,0 +1,15 @@
+/*
+ * Local MIOS32 configuration file
+ *
+ * this file allows to disable (or re-configure) default functions of MIOS32
+ * available switches are listed in $MIOS32_PATH/modules/mios32/MIOS32_CONFIG.txt
+ *
+ */
+
+#ifndef _MIOS32_CONFIG_H
+#define _MIOS32_CONFIG_H
+
+#define MIOS32_LCD_BOOT_MSG_LINE1 "SD-Card r/w Check"
+#define MIOS32_LCD_BOOT_MSG_LINE2 "(C) 2009 M. Maechler"
+
+#endif /* _MIOS32_CONFIG_H */
