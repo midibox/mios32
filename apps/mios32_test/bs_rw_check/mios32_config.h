5c1e17eba0cb03cb7ebcc8e98accbe9548ded132
this <thismaechler@gmail.com>
2009-06-21 03:25:48 +0000

    add

---
+++ apps/mios32_test/bs_rw_check/mios32_config.h
@@ -0,0 +1,19 @@
+/*
+ * Local MIOS32 configuration file
+ *
+ * this file allows to disable (or re-configure) default functions of MIOS32
+ * available switches are listed in $MIOS32_PATH/modules/mios32/MIOS32_CONFIG.txt
+ *
+ */
+
+#ifndef _MIOS32_CONFIG_H
+#define _MIOS32_CONFIG_H
+
+#define MIOS32_LCD_BOOT_MSG_LINE1 "BS Check"
+#define MIOS32_LCD_BOOT_MSG_LINE2 "(C) 2009 M. Maechler"
+
+// enable banksticks
+#define MIOS32_IIC_BS_NUM 8
+
+
+#endif /* _MIOS32_CONFIG_H */
