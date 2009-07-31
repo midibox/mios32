612f0353bfdfe4117f7feb389b9a5acb56326fe3
tk <tk@midibox.org>
2009-07-31 21:55:26 +0000

    added more tutorial lessons

---
+++ apps/tutorials/008_j5_outputs/app.c
@@ -0,0 +1,116 @@
+// $Id$
+/*
+ * MIOS32 Tutorial #008: J5 Outputs
+ * see README.txt for details
+ *
+ * ==========================================================================
+ *
+ *  Copyright (C) 2009 Thorsten Klose (tk@midibox.org)
+ *  Licensed for personal non-commercial use only.
+ *  All other rights reserved.
+ * 
+ * ==========================================================================
+ */
+
+/////////////////////////////////////////////////////////////////////////////
+// Include files
+/////////////////////////////////////////////////////////////////////////////
+
+#include <mios32.h>
+#include "app.h"
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called after startup to initialize the application
+/////////////////////////////////////////////////////////////////////////////
+void APP_Init(void)
+{
+  // initialize all LEDs
+  MIOS32_BOARD_LED_Init(0xffffffff);
+
+  // initialize all pins of J5A, J5B and J5C as outputs in Push-Pull Mode
+  int pin;
+  for(pin=0; pin<12; ++pin)
+    MIOS32_BOARD_J5_PinInit(pin, MIOS32_BOARD_PIN_MODE_OUTPUT_PP);
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This task is running endless in background
+/////////////////////////////////////////////////////////////////////////////
+void APP_Background(void)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when a MIDI package has been received
+/////////////////////////////////////////////////////////////////////////////
+void APP_MIDI_NotifyPackage(mios32_midi_port_t port, mios32_midi_package_t midi_package)
+{
+  // toggle Status LED on each incoming MIDI package
+  MIOS32_BOARD_LED_Set(1, ~MIOS32_BOARD_LED_Get());
+
+  // 1) the LED should be turned on whenever a Note On Event with velocity > 0
+  // has been received
+  if( midi_package.type == NoteOn && midi_package.velocity > 0 ) {
+    // determine LED number (one of 12, each octave)
+    u8 led = midi_package.note % 12;
+    // turn on LED
+    MIOS32_BOARD_J5_PinSet(led, 1);
+  }
+
+  // 2) the LED should be turned off whenever a Note Off Event or a Note On
+  // event with velocity == 0 has been received (the MIDI spec says, that velocity 0
+  // should be handled like Note Off)
+  else if( midi_package.type == NoteOff ||
+	   midi_package.type == NoteOn && midi_package.velocity == 0 ) {
+    // determine LED number (one of 12, each octave)
+    u8 led = midi_package.note % 12;
+    // turn off LED
+    MIOS32_BOARD_J5_PinSet(led, 0);
+  }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called before the shift register chain is scanned
+/////////////////////////////////////////////////////////////////////////////
+void APP_SRIO_ServicePrepare(void)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called after the shift register chain has been scanned
+/////////////////////////////////////////////////////////////////////////////
+void APP_SRIO_ServiceFinish(void)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when a button has been toggled
+// pin_value is 1 when button released, and 0 when button pressed
+/////////////////////////////////////////////////////////////////////////////
+void APP_DIN_NotifyToggle(u32 pin, u32 pin_value)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when an encoder has been moved
+// incrementer is positive when encoder has been turned clockwise, else
+// it is negative
+/////////////////////////////////////////////////////////////////////////////
+void APP_ENC_NotifyChange(u32 encoder, s32 incrementer)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when a pot has been moved
+/////////////////////////////////////////////////////////////////////////////
+void APP_AIN_NotifyChange(u32 pin, u32 pin_value)
+{
+}
