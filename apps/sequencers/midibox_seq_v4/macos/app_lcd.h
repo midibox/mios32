cc9d020d2ea1a4b4fbc5ee2157a50646511ac87c
tk <tk@midibox.org>
2009-11-22 03:28:23 +0000

    MBSEQ: updated emulation

--- apps/sequencers/midibox_seq_v4/macos/app_lcd.h
+++ apps/sequencers/midibox_seq_v4/macos/app_lcd.h
@@ -38,8 +38,10 @@ extern s32 APP_LCD_CursorSet(u16 column, u16 line);
 extern s32 APP_LCD_GCursorSet(u16 x, u16 y);
 extern s32 APP_LCD_PrintChar(char c);
 extern s32 APP_LCD_SpecialCharInit(u8 num, u8 table[8]);
-extern s32 APP_LCD_BColourSet(u8 r, u8 g, u8 b);
-extern s32 APP_LCD_FColourSet(u8 r, u8 g, u8 b);
+extern s32 APP_LCD_BColourSet(u32 rgb);
+extern s32 APP_LCD_FColourSet(u32 rgb);
+extern s32 APP_LCD_BitmapPixelSet(mios32_lcd_bitmap_t bitmap, u16 x, u16 y, u32 colour);
+extern s32 APP_LCD_BitmapPrint(mios32_lcd_bitmap_t bitmap);
 
 
 /////////////////////////////////////////////////////////////////////////////
