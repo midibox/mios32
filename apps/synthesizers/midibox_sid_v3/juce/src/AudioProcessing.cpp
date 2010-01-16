b5d46e7fd2f4b427ba25b737402525ee383db338
tk <tk@midibox.org>
2010-01-16 20:26:50 +0000

    MBSID emulation: added resid, testtone is working

--- apps/synthesizers/midibox_sid_v3/juce/src/AudioProcessing.cpp
+++ apps/synthesizers/midibox_sid_v3/juce/src/AudioProcessing.cpp
@@ -33,6 +33,40 @@
 #include "AudioProcessing.h"
 #include "EditorComponent.h"
 
+#include "../resid/sid.h"
+
+
+// for output to console (stderr)
+// should be at least 1 to inform the user on fatal errors
+#define DEBUG_VERBOSE_LEVEL 1
+
+
+// these global variables are used by ReSID
+double mixer_value1;
+double mixer_value2;
+double mixer_value3;
+
+// number of emulated SID(s)
+// if 0: emulation disabled
+#define SID_NUM 2
+
+#if SID_NUM
+// TK: note that static memory allocation is used here
+// auval fails if reSID is dynamically allocated/deallocated.
+// Sometimes the object is accessed after it has been deallocated, this seems to be a bug in MacOS 10.6? 
+SID reSID[SID_NUM];
+#endif
+
+// sampling method
+#define RESID_SAMPLING_METHOD SAMPLE_RESAMPLE_INTERPOLATE
+//#define RESID_SAMPLING_METHOD SAMPLE_FAST
+
+// play testtone at startup?
+// nice for first checks of the emulation w/o MIDI input
+#define RESID_PLAY_TESTTONE 1
+
+int reSidEnabled;
+
 
 //==============================================================================
 /**
@@ -47,14 +81,38 @@ AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 //==============================================================================
 AudioProcessing::AudioProcessing()
 {
-    gain = 1.0f;
-    lastUIWidth = 400;
-    lastUIHeight = 140;
-
-    zeromem (&lastPosInfo, sizeof (lastPosInfo));
-    lastPosInfo.timeSigNumerator = 4;
-    lastPosInfo.timeSigDenominator = 4;
-    lastPosInfo.bpm = 120;
+  gain = 1.0f;
+  lastUIWidth = 400;
+  lastUIHeight = 140;
+  
+  zeromem (&lastPosInfo, sizeof (lastPosInfo));
+  lastPosInfo.timeSigNumerator = 4;
+  lastPosInfo.timeSigDenominator = 4;
+  lastPosInfo.bpm = 120;
+  
+  // reSID integration
+  mixer_value1 = 1.0f;
+  mixer_value2 = 1.0f;
+  mixer_value3 = 1.0f;
+  
+#if SID_NUM
+  reSidEnabled = 1;
+  for(int i=0; i<SID_NUM; ++i) {
+    reSID[i].reset();
+    double freq = 44100.0;
+    if( !reSID[i].set_sampling_parameters(1000000, RESID_SAMPLING_METHOD, freq) ) {
+#if DEBUG_VERBOSE_LEVEL >= 1
+      fprintf(stderr, "Initialisation of reSID[%d] failed for unexpected reasons! All SIDs disabled now!\n", i);
+#endif
+      reSidEnabled = 0;
+    }
+  }
+#else
+  reSidEnabled = 0;
+#if DEBUG_VERBOSE_LEVEL >= 1
+  fprintf(stderr, "SID_NUM == 0: emulation disabled!\n");
+#endif
+#endif
 }
 
 AudioProcessing::~AudioProcessing()
@@ -64,188 +122,266 @@ AudioProcessing::~AudioProcessing()
 //==============================================================================
 const String AudioProcessing::getName() const
 {
-    return "Juce Demo Filter";
+  return "Juce Demo Filter";
 }
 
 int AudioProcessing::getNumParameters()
 {
-    return 1;
+  return 1;
 }
 
 float AudioProcessing::getParameter (int index)
 {
-    return (index == 0) ? gain
-                        : 0.0f;
+  return (index == 0) ? gain
+		      : 0.0f;
 }
 
 void AudioProcessing::setParameter (int index, float newValue)
 {
-    if (index == 0)
-    {
-        if (gain != newValue)
-        {
-            gain = newValue;
+  switch( index ) {
+    case 0:
+      if( gain != newValue ) {
+	gain = newValue;
 
-            // if this is changing the gain, broadcast a change message which
-            // our editor will pick up.
-            sendChangeMessage (this);
-        }
-    }
+	// if this is changing the gain, broadcast a change message which
+	// our editor will pick up.
+	sendChangeMessage (this);
+      }
+  }
 }
 
 const String AudioProcessing::getParameterName (int index)
 {
-    if (index == 0)
-        return T("gain");
+  switch( index ) {
+    case 0: return T("gain");
+  }
 
-    return String::empty;
+  return String::empty;
 }
 
 const String AudioProcessing::getParameterText (int index)
 {
-    if (index == 0)
-        return String (gain, 2);
-
-    return String::empty;
+  switch( index ) {
+    case 0: return String (gain, 2);
+  }
+  
+  return String::empty;
 }
 
 const String AudioProcessing::getInputChannelName (const int channelIndex) const
 {
-    return String (channelIndex + 1);
+  return String (channelIndex + 1);
 }
 
 const String AudioProcessing::getOutputChannelName (const int channelIndex) const
 {
-    return String (channelIndex + 1);
+  return String (channelIndex + 1);
 }
 
 bool AudioProcessing::isInputChannelStereoPair (int index) const
 {
-    return false;
+  return false;
 }
 
 bool AudioProcessing::isOutputChannelStereoPair (int index) const
 {
-    return false;
+  return false;
 }
 
 bool AudioProcessing::acceptsMidi() const
 {
-    return true;
+  return true;
 }
 
 bool AudioProcessing::producesMidi() const
 {
-    return true;
+  return true;
 }
 
 //==============================================================================
 void AudioProcessing::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // do your pre-playback setup stuff here..
-    keyboardState.reset();
+  // do your pre-playback setup stuff here..
+  keyboardState.reset();
+
+#if SID_NUM
+  reSidEnabled = 1;
+  for(int i=0; i<SID_NUM; ++i) {
+    reSID[i].reset();
+    if( !reSID[i].set_sampling_parameters(1000000, RESID_SAMPLING_METHOD, sampleRate) ) {
+#if DEBUG_VERBOSE_LEVEL >= 1
+      fprintf(stderr, "Initialisation of reSID[%d] failed at sample rate %7.2f Hz! All SIDs disabled now!\n", i, sampleRate);
+#endif
+      reSidEnabled = 0;
+    }
+
+#if RESID_PLAY_TESTTONE
+    if( reSidEnabled ) {
+#if DEBUG_VERBOSE_LEVEL >= 2
+      fprintf(stderr, "playing testtone on SID %d\n", i);
+#endif
+      reSID[i].write(0x18, 0x0f); // Volume
+      reSID[i].write(0x05, 0x00); // Attack/Decay
+      reSID[i].write(0x06, 0xf8); // Sustain/Release
+      reSID[i].write(0x00, 16777 & 0xff); // Frequency Low
+      reSID[i].write(0x01, 16777 >> 8); // Frequency High
+      reSID[i].write(0x04, 0x11); // Waveform + Gate
+    }
+#endif
+  }
+#endif
 }
 
 void AudioProcessing::releaseResources()
 {
-    // when playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
+  // when playback stops, you can use this as an opportunity to free up any
+  // spare memory, etc.
 }
 
 void AudioProcessing::processBlock (AudioSampleBuffer& buffer,
-                                   MidiBuffer& midiMessages)
+				    MidiBuffer& midiMessages)
 {
-    // for each of our input channels, we'll attenuate its level by the
-    // amount that our volume parameter is set to.
-    for (int channel = 0; channel < getNumInputChannels(); ++channel)
-    {
-        buffer.applyGain (channel, 0, buffer.getNumSamples(), gain);
-    }
+  // determine how many channels have to be services
+  int numChannels = getNumInputChannels();
 
-    // in case we have more outputs than inputs, we'll clear any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    for (int i = getNumInputChannels(); i < getNumOutputChannels(); ++i)
-    {
-        buffer.clear (i, 0, buffer.getNumSamples());
-    }
+#if SID_NUM
+  if( reSidEnabled ) {
+    if( numChannels < SID_NUM )
+      numChannels = SID_NUM;
+    if( numChannels > getNumOutputChannels() )
+      numChannels = getNumOutputChannels();
+    
+    // number of samples which have to be rendered
+    int numSamples = buffer.getNumSamples();
+    
+    // TODO: delta clocks depend on sample rate
+    cycle_count delta_t = 23;
 
-    // if any midi messages come in, use them to update the keyboard state object. This
-    // object sends notification to the UI component about key up/down changes
-    keyboardState.processNextMidiBuffer (midiMessages,
-                                         0, buffer.getNumSamples(),
-                                         true);
-
-    // have a go at getting the current time from the host, and if it's changed, tell
-    // our UI to update itself.
-    AudioPlayHead::CurrentPositionInfo pos;
-
-    if (getPlayHead() != 0 && getPlayHead()->getCurrentPosition (pos))
-    {
-        if (memcmp (&pos, &lastPosInfo, sizeof (pos)) != 0)
-        {
-            lastPosInfo = pos;
-            sendChangeMessage (this);
-        }
+    // add SID sound(s) to output(s)
+    // TK: this nested loop isn't optimal for CPU load, but we have to ensure that all SIDs are in lock-step
+    for(int i=0; i<numSamples; ++i) {
+      for(int channel = 0; channel < numChannels; ++channel) {
+	reSID[channel].clock(delta_t);
+	float currentSample = (float)reSID[channel].output(16) / 32768.0;
+	*buffer.getSampleData(channel, i) = currentSample;
+      }
     }
-    else
-    {
-        zeromem (&lastPosInfo, sizeof (lastPosInfo));
-        lastPosInfo.timeSigNumerator = 4;
-        lastPosInfo.timeSigDenominator = 4;
-        lastPosInfo.bpm = 120;
+  }
+#endif
+
+  // for each of our input channels, we'll attenuate its level by the
+  // amount that our volume parameter is set to.
+  for (int channel = 0; channel < numChannels; ++channel) {
+    buffer.applyGain (channel, 0, buffer.getNumSamples(), gain);
+  }
+  
+  // in case we have more outputs than inputs, we'll clear any output
+  // channels that didn't contain input data, (because these aren't
+  // guaranteed to be empty - they may contain garbage).
+  for (int i = numChannels; i < getNumOutputChannels(); ++i) {
+    buffer.clear (i, 0, buffer.getNumSamples());
+  }
+  
+  // if any midi messages come in, use them to update the keyboard state object. This
+  // object sends notification to the UI component about key up/down changes
+  keyboardState.processNextMidiBuffer (midiMessages,
+				       0, buffer.getNumSamples(),
+				       true);
+  
+  // have a go at getting the current time from the host, and if it's changed, tell
+  // our UI to update itself.
+  AudioPlayHead::CurrentPositionInfo pos;
+  
+  if (getPlayHead() != 0 && getPlayHead()->getCurrentPosition (pos)) {
+    if (memcmp (&pos, &lastPosInfo, sizeof (pos)) != 0) {
+      lastPosInfo = pos;
+      sendChangeMessage (this);
     }
+  } else {
+    zeromem (&lastPosInfo, sizeof (lastPosInfo));
+    lastPosInfo.timeSigNumerator = 4;
+    lastPosInfo.timeSigDenominator = 4;
+    lastPosInfo.bpm = 120;
+  }
 }
 
 //==============================================================================
 AudioProcessorEditor* AudioProcessing::createEditor()
 {
-    return new EditorComponent (this);
+  return new EditorComponent (this);
 }
 
 //==============================================================================
 void AudioProcessing::getStateInformation (MemoryBlock& destData)
 {
-    // you can store your parameters as binary data if you want to or if you've got
-    // a load of binary to put in there, but if you're not doing anything too heavy,
-    // XML is a much cleaner way of doing it - here's an example of how to store your
-    // params as XML..
-
-    // create an outer XML element..
-    XmlElement xmlState (T("MYPLUGINSETTINGS"));
-
-    // add some attributes to it..
-    xmlState.setAttribute (T("pluginVersion"), 1);
-    xmlState.setAttribute (T("gainLevel"), gain);
-    xmlState.setAttribute (T("uiWidth"), lastUIWidth);
-    xmlState.setAttribute (T("uiHeight"), lastUIHeight);
-
-    // you could also add as many child elements as you need to here..
-
-
-    // then use this helper function to stuff it into the binary blob and return it..
-    copyXmlToBinary (xmlState, destData);
+  // you can store your parameters as binary data if you want to or if you've got
+  // a load of binary to put in there, but if you're not doing anything too heavy,
+  // XML is a much cleaner way of doing it - here's an example of how to store your
+  // params as XML..
+  
+  // create an outer XML element..
+  XmlElement xmlState (T("MYPLUGINSETTINGS"));
+  
+  // add some attributes to it..
+  xmlState.setAttribute (T("pluginVersion"), 1);
+  xmlState.setAttribute (T("gainLevel"), gain);
+  xmlState.setAttribute (T("uiWidth"), lastUIWidth);
+  xmlState.setAttribute (T("uiHeight"), lastUIHeight);
+  
+  // you could also add as many child elements as you need to here..
+  
+  
+  // then use this helper function to stuff it into the binary blob and return it..
+  copyXmlToBinary (xmlState, destData);
 }
 
 void AudioProcessing::setStateInformation (const void* data, int sizeInBytes)
 {
-    // use this helper function to get the XML from this binary blob..
-    XmlElement* const xmlState = getXmlFromBinary (data, sizeInBytes);
-
-    if (xmlState != 0)
+  // use this helper function to get the XML from this binary blob..
+  XmlElement* const xmlState = getXmlFromBinary (data, sizeInBytes);
+  
+  if (xmlState != 0)
+  {
+    // check that it's the right type of xml..
+    if (xmlState->hasTagName (T("MYPLUGINSETTINGS")))
     {
-        // check that it's the right type of xml..
-        if (xmlState->hasTagName (T("MYPLUGINSETTINGS")))
-        {
-            // ok, now pull out our parameters..
-            gain = (float) xmlState->getDoubleAttribute (T("gainLevel"), gain);
-
-            lastUIWidth = xmlState->getIntAttribute (T("uiWidth"), lastUIWidth);
-            lastUIHeight = xmlState->getIntAttribute (T("uiHeight"), lastUIHeight);
-
-            sendChangeMessage (this);
-        }
-
-        delete xmlState;
+      // ok, now pull out our parameters..
+      gain = (float) xmlState->getDoubleAttribute (T("gainLevel"), gain);
+      
+      lastUIWidth = xmlState->getIntAttribute (T("uiWidth"), lastUIWidth);
+      lastUIHeight = xmlState->getIntAttribute (T("uiHeight"), lastUIHeight);
+      
+      sendChangeMessage (this);
     }
+    
+    delete xmlState;
+  }
 }
+
+
+
+// Accessible for C
+extern "C" {
+  void SID_Wrapper_Write(unsigned char cs, unsigned char addr, unsigned char data, unsigned char reset)
+  {
+#if SID_NUM
+    if( reSidEnabled ) {
+      int i;
+      
+#if DEBUG_VERBOSE_LEVEL >= 2
+      fprintf(stderr, "cs%d %02x:%02x\n", cs, addr, data);
+#endif
+      if( reset ) {
+	for(i=0; i<SID_NUM; ++i)
+	  reSID[i].reset();
+      } else {
+	for(i=0; i<SID_NUM; ++i) {
+	  if( cs & (1 << i) )
+	    reSID[0].write(addr, data);
+	}
+      }
+    }
+#endif
+  }
+}
+
