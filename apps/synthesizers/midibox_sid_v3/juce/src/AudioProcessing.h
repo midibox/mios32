f17c133e00b18a208c112bc9ce1921d4a9978237
tk <tk@midibox.org>
2010-01-16 22:35:04 +0000

    MBSID resid integration: fixed OO integration

--- apps/synthesizers/midibox_sid_v3/juce/src/AudioProcessing.h
+++ apps/synthesizers/midibox_sid_v3/juce/src/AudioProcessing.h
@@ -32,6 +32,13 @@
 #ifndef AUDIO_PROCESSING_H
 #define AUDIO_PROCESSING_H
 
+#include "../resid/resid.h"
+
+
+// number of emulated SID(s)
+// if 0: emulation disabled
+#define SID_NUM 2
+
 
 //==============================================================================
 /**
@@ -40,77 +47,87 @@
 
 */
 class AudioProcessing  : public AudioProcessor,
-                        public ChangeBroadcaster
+public ChangeBroadcaster
 {
 public:
-    //==============================================================================
-    AudioProcessing();
-    ~AudioProcessing();
+  //==============================================================================
+  AudioProcessing();
+  ~AudioProcessing();
+  
+  //==============================================================================
+  void prepareToPlay (double sampleRate, int samplesPerBlock);
+  void releaseResources();
+  
+  void processBlock (AudioSampleBuffer& buffer,
+		     MidiBuffer& midiMessages);
+  
+  //==============================================================================
+  AudioProcessorEditor* createEditor();
+  
+  //==============================================================================
+  const String getName() const;
+  
+  int getNumParameters();
+  
+  float getParameter (int index);
+  void setParameter (int index, float newValue);
+  
+  const String getParameterName (int index);
+  const String getParameterText (int index);
+  
+  const String getInputChannelName (const int channelIndex) const;
+  const String getOutputChannelName (const int channelIndex) const;
+  bool isInputChannelStereoPair (int index) const;
+  bool isOutputChannelStereoPair (int index) const;
+  
+  bool acceptsMidi() const;
+  bool producesMidi() const;
+  
+  //==============================================================================
+  int getNumPrograms()                                        { return 0; }
+  int getCurrentProgram()                                     { return 0; }
+  void setCurrentProgram (int index)                          { }
+  const String getProgramName (int index)                     { return String::empty; }
+  void changeProgramName (int index, const String& newName)   { }
+  
+  //==============================================================================
+  void getStateInformation (MemoryBlock& destData);
+  void setStateInformation (const void* data, int sizeInBytes);
+  
+  //==============================================================================
+  // These properties are public so that our editor component can access them
+  //  - a bit of a hacky way to do it, but it's only a demo!
+  
+  // this is kept up to date with the midi messages that arrive, and the UI component
+  // registers with it so it can represent the incoming messages
+  MidiKeyboardState keyboardState;
+  
+  // this keeps a copy of the last set of time info that was acquired during an audio
+  // callback - the UI component will read this and display it.
+  AudioPlayHead::CurrentPositionInfo lastPosInfo;
+  
+  // these are used to persist the UI's size - the values are stored along with the
+  // filter's other parameters, and the UI component will update them when it gets
+  // resized.
+  int lastUIWidth, lastUIHeight;
 
-    //==============================================================================
-    void prepareToPlay (double sampleRate, int samplesPerBlock);
-    void releaseResources();
-
-	void processBlock (AudioSampleBuffer& buffer,
-                       MidiBuffer& midiMessages);
-
-    //==============================================================================
-    AudioProcessorEditor* createEditor();
-
-    //==============================================================================
-    const String getName() const;
-
-    int getNumParameters();
-
-    float getParameter (int index);
-    void setParameter (int index, float newValue);
-
-    const String getParameterName (int index);
-    const String getParameterText (int index);
-
-    const String getInputChannelName (const int channelIndex) const;
-    const String getOutputChannelName (const int channelIndex) const;
-    bool isInputChannelStereoPair (int index) const;
-    bool isOutputChannelStereoPair (int index) const;
-
-    bool acceptsMidi() const;
-    bool producesMidi() const;
-
-    //==============================================================================
-    int getNumPrograms()                                        { return 0; }
-    int getCurrentProgram()                                     { return 0; }
-    void setCurrentProgram (int index)                          { }
-    const String getProgramName (int index)                     { return String::empty; }
-    void changeProgramName (int index, const String& newName)   { }
-
-    //==============================================================================
-    void getStateInformation (MemoryBlock& destData);
-    void setStateInformation (const void* data, int sizeInBytes);
-
-    //==============================================================================
-    // These properties are public so that our editor component can access them
-    //  - a bit of a hacky way to do it, but it's only a demo!
-
-    // this is kept up to date with the midi messages that arrive, and the UI component
-    // registers with it so it can represent the incoming messages
-    MidiKeyboardState keyboardState;
-
-    // this keeps a copy of the last set of time info that was acquired during an audio
-    // callback - the UI component will read this and display it.
-    AudioPlayHead::CurrentPositionInfo lastPosInfo;
-
-    // these are used to persist the UI's size - the values are stored along with the
-    // filter's other parameters, and the UI component will update them when it gets
-    // resized.
-    int lastUIWidth, lastUIHeight;
-
-    //==============================================================================
-    juce_UseDebuggingNewOperator
-
+  
+#if SID_NUM
+  SID *reSID[SID_NUM];
+#endif
+  
+  int reSidEnabled;
+  double reSidSampleRate;
+  double reSidDeltaCycleCounter;
+  
+  
+  //==============================================================================
+  juce_UseDebuggingNewOperator
+  
 private:
-    // this is our gain - the UI and the host can access this by getting/setting
-    // parameter 0.
-    float gain;
+  // this is our gain - the UI and the host can access this by getting/setting
+  // parameter 0.
+  float gain;
 };
 
 
