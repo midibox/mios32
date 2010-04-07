d5d694fc7faf8d3fde7656e2c9a423923edca188
philetaylor <phil.taylor@lansystems.co.uk>
2010-04-07 18:48:20 +0000

    Change memory allocation to use umm_malloc instead of heap_2

--- programming_models/traditional/programming_model.mk
+++ programming_models/traditional/programming_model.mk
@@ -1,13 +1,17 @@
 # $Id$
 # defines rules building the programming model
 
+# philetaylor - changed to use umm_malloc and added MemMang to the include dirs.
+
 # where is FreeRTOS located
+
 FREE_RTOS      =    $(MIOS32_PATH)/FreeRTOS
 
 # extend include path
 C_INCLUDE += 	-I $(MIOS32_PATH)/programming_models/traditional \
 		-I $(FREE_RTOS)/Source/include \
-		-I $(FREE_RTOS)/Source/portable/GCC/ARM_CM3
+		-I $(FREE_RTOS)/Source/portable/GCC/ARM_CM3 \
+		-I $(FREE_RTOS)/Source/portable/MemMang \
 
 # required by FreeRTOS to select the port
 CFLAGS    +=    -DGCC_ARMCM3
@@ -20,7 +24,7 @@ THUMB_SOURCE += \
 		$(FREE_RTOS)/Source/list.c \
 		$(FREE_RTOS)/Source/queue.c \
 		$(FREE_RTOS)/Source/portable/GCC/ARM_CM3/port.c \
-		$(FREE_RTOS)/Source/portable/MemMang/heap_2.c 
+		$(FREE_RTOS)/Source/portable/MemMang/umm_malloc.c 
 
 THUMB_CPP_SOURCE += $(MIOS32_PATH)/programming_models/traditional/mini_cpp.cpp \
 		    $(MIOS32_PATH)/programming_models/traditional/freertos_heap.cpp
