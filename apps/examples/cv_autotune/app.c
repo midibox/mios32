05de97c9ba94a00f132ced8b91960ce9efb0a846
tk <tk@midibox.org>
2011-03-13 17:17:38 +0000

    implemented CV Autotune application

---
+++ apps/examples/cv_autotune/app.c
@@ -0,0 +1,377 @@
+// $Id$
+/*
+ * CV Autotune
+ *
+ * ==========================================================================
+ *
+ *  Copyright (C) 2011 Thorsten Klose (tk@midibox.org)
+ *  Licensed for personal non-commercial use only.
+ *  All other rights reserved.
+ * 
+ * ==========================================================================
+ */
+
+/////////////////////////////////////////////////////////////////////////////
+// Include files
+/////////////////////////////////////////////////////////////////////////////
+
+#include <mios32.h>
+#include "app.h"
+#include "frq_meter.h"
+
+#include <notestack.h>
+#include <aout.h>
+
+#include <FreeRTOS.h>
+#include <portmacro.h>
+#include <task.h>
+#include <queue.h>
+#include <semphr.h>
+
+
+// define priority level for FRQ_MEASURE task:
+// use same priority as MIOS32 specific tasks (3)
+#define PRIORITY_TASK_FRQ_MEASURE	( tskIDLE_PRIORITY + 3 )
+
+// local prototype of the task function
+static void TASK_Frq_Measure(void *pvParameters);
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Local definitions
+/////////////////////////////////////////////////////////////////////////////
+
+#define NOTESTACK_SIZE 16
+
+#define AUTOTUNE_STATE_NOP        0
+#define AUTOTUNE_STATE_START      1
+#define AUTOTUNE_STATE_SWEEP      2
+#define AUTOTUNE_STATE_PASSED     3
+#define AUTOTUNE_STATE_FAILED     4
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Local variables
+/////////////////////////////////////////////////////////////////////////////
+
+static notestack_t notestack;
+static notestack_item_t notestack_items[NOTESTACK_SIZE];
+
+static volatile u8 autotune_state;
+static u32 autotune_last_aout_value;
+static u8 autotune_current_note;
+static u16 autotune_note_aout_value[128];
+static u32 autotune_note_frq[128];
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called after startup to initialize the application
+/////////////////////////////////////////////////////////////////////////////
+void APP_Init(void)
+{
+  // initialize all LEDs
+  MIOS32_BOARD_LED_Init(0xffffffff);
+
+  // initialize frequency meter
+  FRQ_METER_Init(0);
+
+  // initialize the Notestack
+  NOTESTACK_Init(&notestack, NOTESTACK_MODE_PUSH_TOP, &notestack_items[0], NOTESTACK_SIZE);
+
+  // initialize AOUT module
+  AOUT_Init(0);
+
+  // configure interface
+  // see AOUT module documentation for available interfaces and options
+  aout_config_t config;
+  config = AOUT_ConfigGet();
+  config.if_type = AOUT_IF_TLV5630;
+  config.if_option = 0;
+  config.num_channels = 8;
+  config.chn_inverted = 0;
+  AOUT_ConfigSet(config);
+  AOUT_IF_Init(0);
+
+  // clear autotune state
+  autotune_state = AUTOTUNE_STATE_NOP;
+
+  // calculate note frequencies
+  float fact = 1.059463;
+  float a_freq = 13.75;
+  int i;
+  for(i=0; i<128; i+=12, a_freq *= 2) {
+    int j;
+    float note_frq[12];
+
+    note_frq[9] = a_freq;
+    for(j=10; j<=11; ++j)
+      note_frq[j] = note_frq[j-1] * fact;
+    for(j=8; j>=0; --j)
+      note_frq[j] = note_frq[j+1] / fact;
+
+    for(j=0; j<12 && (j+i)<128; ++j) {
+      autotune_note_frq[i+j] = (int)(note_frq[j] * 1000);
+      autotune_note_aout_value[i+j] = (i+j)*0x200; // default for V/Oct
+    }
+  }
+
+  // start task
+  xTaskCreate(TASK_Frq_Measure, (signed portCHAR *)"Frq_Measure", configMINIMAL_STACK_SIZE, NULL, PRIORITY_TASK_FRQ_MEASURE, NULL);
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This task is running endless in background
+/////////////////////////////////////////////////////////////////////////////
+void APP_Background(void)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when a MIDI package has been received
+/////////////////////////////////////////////////////////////////////////////
+void APP_MIDI_NotifyPackage(mios32_midi_port_t port, mios32_midi_package_t midi_package)
+{
+  // Note On received?
+  if( midi_package.chn == Chn1 && 
+      (midi_package.type == NoteOn || midi_package.type == NoteOff) ) {
+
+    // skip if autotune running
+    if( midi_package.type == NoteOn && midi_package.velocity > 0 ) {
+      if( autotune_state != AUTOTUNE_STATE_NOP ) {
+	MIOS32_MIDI_SendDebugMessage("Autotune running - Note ignored!\n");
+	return;
+      }
+
+      // if Note c-2 is played: activate autotune
+      if( midi_package.note == 0x00 ) {
+	autotune_state = AUTOTUNE_STATE_START;
+	return;
+      }
+    }
+
+
+    // branch depending on Note On/Off event
+    if( midi_package.event == NoteOn && midi_package.velocity > 0 ) {
+      // push note into note stack
+      NOTESTACK_Push(&notestack, midi_package.note, midi_package.velocity);
+    } else {
+      // remove note from note stack
+      NOTESTACK_Pop(&notestack, midi_package.note);
+    }
+
+
+    // still a note in stack?
+    if( notestack.len ) {
+      // take first note of stack
+      // we have to convert the 7bit value to a 16bit value
+      //u16 note_cv = notestack_items[0].note << 9;
+      u16 note_cv = autotune_note_aout_value[notestack_items[0].note];
+      u16 velocity_cv = notestack_items[0].tag << 9;
+
+      // change voltages
+      AOUT_PinSet(0, note_cv);
+      AOUT_PinSet(1, velocity_cv);
+
+      // set board LED
+      MIOS32_BOARD_LED_Set(1, 1);
+
+      // set AOUT digital output (if available)
+      AOUT_DigitalPinSet(0, 1);
+    } else {
+      // turn off LED (can also be used as a gate output!)
+      MIOS32_BOARD_LED_Set(1, 0);
+
+      // clear AOUT digital output (if available)
+      AOUT_DigitalPinSet(0, 0);
+    }
+
+    // update AOUT channel(s)
+    AOUT_Update();
+
+#if 1
+    // optional debug messages
+    NOTESTACK_SendDebugMessage(&notestack);
+#endif
+
+  // CC received?
+  } else if( midi_package.chn == Chn1 && midi_package.type == CC ) {
+
+    // we have to convert the 7bit value to a 16bit value
+    u16 cc_cv = midi_package.value << 9;
+
+    switch( midi_package.cc_number ) {
+      case 16: AOUT_PinSet(2, cc_cv); break;
+      case 17: AOUT_PinSet(3, cc_cv); break;
+      case 18: AOUT_PinSet(4, cc_cv); break;
+      case 19: AOUT_PinSet(5, cc_cv); break;
+      case 20: AOUT_PinSet(6, cc_cv); break;
+      case 21: AOUT_PinSet(7, cc_cv); break;
+    }
+
+    // update AOUT channel(s)
+    // register upload will be omitted automatically if no channel value has changed
+    AOUT_Update();
+  }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called before the shift register chain is scanned
+/////////////////////////////////////////////////////////////////////////////
+void APP_SRIO_ServicePrepare(void)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called after the shift register chain has been scanned
+/////////////////////////////////////////////////////////////////////////////
+void APP_SRIO_ServiceFinish(void)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when a button has been toggled
+// pin_value is 1 when button released, and 0 when button pressed
+/////////////////////////////////////////////////////////////////////////////
+void APP_DIN_NotifyToggle(u32 pin, u32 pin_value)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when an encoder has been moved
+// incrementer is positive when encoder has been turned clockwise, else
+// it is negative
+/////////////////////////////////////////////////////////////////////////////
+void APP_ENC_NotifyChange(u32 encoder, s32 incrementer)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when a pot has been moved
+/////////////////////////////////////////////////////////////////////////////
+void APP_AIN_NotifyChange(u32 pin, u32 pin_value)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This task measures the frequency
+/////////////////////////////////////////////////////////////////////////////
+static void TASK_Frq_Measure(void *pvParameters)
+{
+  portTickType xLastExecutionTime;
+  u8 frq_was_too_low = 0;
+  u8 frq_was_too_high = 0;
+  u32 last_frq = 0;
+  u32 last_displayed_frq = 0;
+
+  // Initialise the xLastExecutionTime variable on task entry
+  xLastExecutionTime = xTaskGetTickCount();
+
+  while( 1 ) {
+    vTaskDelayUntil(&xLastExecutionTime, 1 / portTICK_RATE_MS);
+
+    u32 frq = FRQ_METER_Tick();
+
+    if( frq > FRQ_METER_TOO_LOW && frq < FRQ_METER_TOO_HIGH ) {
+
+      // print new frequency if it changed more than 0.5%
+      u32 frq_change = abs(frq-last_displayed_frq);
+      u32 permill = (1000*frq_change) / frq;
+      if( permill >= 5 ) {
+	if( autotune_state == AUTOTUNE_STATE_NOP )
+	  MIOS32_MIDI_SendDebugMessage("-> %3d.%02d Hz\n", frq / 1000, (frq % 1000) / 10);
+	last_displayed_frq = frq;
+      }
+
+      // toggle Status LED to as a sign of live
+      MIOS32_BOARD_LED_Set(1, ~MIOS32_BOARD_LED_Get());
+    } else {      
+      if( frq != FRQ_METER_NO_UPDATE ) {
+	if( frq == FRQ_METER_TOO_LOW )
+	  ++frq_was_too_low;
+	else
+	  ++frq_was_too_high;
+
+	if( frq_was_too_low >= 2 || frq_was_too_high >= 10 ) {
+	  if( autotune_state == AUTOTUNE_STATE_NOP ) {
+	    if( frq_was_too_low && !frq_was_too_high )
+	      MIOS32_MIDI_SendDebugMessage("-> frequency too low!\n");
+	    else if( frq_was_too_high && !frq_was_too_low )
+	      MIOS32_MIDI_SendDebugMessage("-> frequency too high!\n");
+	    else
+	      MIOS32_MIDI_SendDebugMessage("-> frequency very unstable!\n");
+	  }
+
+	  frq_was_too_low = 0;
+	  frq_was_too_high = 0;
+
+	  last_displayed_frq = frq;
+	}
+      }
+    }
+
+    // autotune
+    if( frq != FRQ_METER_NO_UPDATE ) {
+      switch( autotune_state ) {
+      case AUTOTUNE_STATE_START:
+	MIOS32_MIDI_SendDebugMessage("Autotune started.\n");
+	autotune_last_aout_value = 0;
+	autotune_current_note = 0;
+	AOUT_PinSet(0, autotune_last_aout_value);
+	AOUT_Update();
+	autotune_state = AUTOTUNE_STATE_SWEEP;
+	break;
+
+      case AUTOTUNE_STATE_SWEEP: {
+	u32 search_frq = autotune_note_frq[autotune_current_note];
+	MIOS32_MIDI_SendDebugMessage("0x%03x: %3d.%02d Hz (searching for #%d: %3d.%02d Hz)\n",
+				     autotune_last_aout_value,
+				     frq / 1000, (frq % 1000) / 10,
+				     autotune_current_note,
+				     search_frq / 1000, (search_frq % 1000) / 10);
+
+	if( frq >= search_frq ) {
+	  autotune_note_aout_value[autotune_current_note] = autotune_last_aout_value;
+	  if( ++autotune_current_note >= 0x80 ) {
+	    autotune_state = AUTOTUNE_STATE_PASSED;
+	  }
+	} else {
+	  if( autotune_last_aout_value >= 0xfff0 ) {
+	    autotune_state = AUTOTUNE_STATE_FAILED;
+	  } else {
+	    autotune_last_aout_value += 0x10;
+	    AOUT_PinSet(0, autotune_last_aout_value);
+	    AOUT_Update();
+	  }
+	}
+      } break;
+
+      case AUTOTUNE_STATE_PASSED:
+	AOUT_PinSet(0, 0x0000);
+	AOUT_Update();
+	autotune_state = AUTOTUNE_STATE_NOP;
+	MIOS32_MIDI_SendDebugMessage("Autotune finished!\n");
+	break;
+
+      case AUTOTUNE_STATE_FAILED:
+	AOUT_PinSet(0, 0x0000);
+	AOUT_Update();
+	autotune_state = AUTOTUNE_STATE_NOP;
+	MIOS32_MIDI_SendDebugMessage("Autotune failed, frequency for note >= %d not assigned!\n", autotune_current_note);
+	break;
+      }
+    }
+
+
+    // store current frequency
+    if( frq != FRQ_METER_NO_UPDATE )
+      last_frq = frq;
+  }
+}
---
+++ apps/examples/cv_autotune/frq_meter.c
@@ -0,0 +1,202 @@
+// $Id$
+/*
+ * Frequency Meter
+ *
+ * ==========================================================================
+ *
+ *  Copyright (C) 2011 Thorsten Klose (tk@midibox.org)
+ *  Licensed for personal non-commercial use only.
+ *  All other rights reserved.
+ * 
+ * ==========================================================================
+ */
+
+/////////////////////////////////////////////////////////////////////////////
+// Include files
+/////////////////////////////////////////////////////////////////////////////
+
+#include <mios32.h>
+#include "frq_meter.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// Global Variables
+/////////////////////////////////////////////////////////////////////////////
+
+u32 frq_meter_current_frq;
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Local Variables
+/////////////////////////////////////////////////////////////////////////////
+
+// how many frequency captures to calculate the mean value?
+#define NUM_CAP_SAMPLES 4
+
+// samples and sample counter
+static u16 cap_samples[NUM_CAP_SAMPLES];
+static u16 cap_sample_ctr;
+static u16 timeout_ctr;
+static u8 frq_updated;
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Initializes the frequency meter
+/////////////////////////////////////////////////////////////////////////////
+s32 FRQ_METER_Init(u32 mode)
+{
+  // clear the sample counter
+  cap_sample_ctr = 0;
+  timeout_ctr = 0;
+  frq_updated = 0;
+
+  frq_meter_current_frq = FRQ_METER_TOO_LOW;
+
+  // Timer configuration (we use TIM2)
+  // We use TIM2, note that it is normaly assigned to MIOS32_TIMER0 (don't use it in your app)
+
+  // enable peripheral clock
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+  // ensure that interrupt is disabled
+  TIM_ITConfig(TIM2, TIM_IT_Update, DISABLE);
+
+  // configure PA0 (J5B.A4) as digital input
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; // internal pull-up enabled
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  // time base configuration
+  // we use a prescaler value of 32, this results into 2.25 MHz timer clock
+  // Due to the 16 bit timer resolution the lowest frequency which can be measured is 34.3 Hz
+  // The highest frequency is ca. 20 kHz (based on input filter value)
+  TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+  TIM_TimeBaseStructure.TIM_Period = 65535; // full range
+  TIM_TimeBaseStructure.TIM_Prescaler = 32-1; // -> 72 MHz / 32 = 2.25 MHz
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+  // Overrun detection: the SR.UIF flag (update) can't be used, as it is set on any update, also on slave request
+  // CR.URS=1 doesn't help, as it doesn't allow the slave channel to reset the timer.
+  // This seems to be a conceptional flaw...
+  // However, we can "fake" the flag by setting the compare value of an unused channel to maximum
+  TIM_SetCompare4(TIM2, 0xffff);
+
+  // TIM configuration: PWM input mode
+  // External signal is connected to TIM2 CH1 pin PA0
+  // Falling edge used as active edge (thats better since input is driven by open-drain driver)
+  // TIM CCR1 is used to compute the frequency value
+  // TIM CCR2 is used to compute the duty cycle value
+  TIM_ICInitTypeDef  TIM_ICInitStructure;
+  TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
+  TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Falling;
+  TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+  TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+  TIM_ICInitStructure.TIM_ICFilter = 0x8; // increased value for stable results
+  TIM_PWMIConfig(TIM2, &TIM_ICInitStructure);
+
+  // Select the TIM2 Input Trigger: TI1FP1
+  TIM_SelectInputTrigger(TIM2, TIM_TS_TI1FP1);
+
+  // Select the slave Mode: Reset Mode
+  TIM_SelectSlaveMode(TIM2, TIM_SlaveMode_Reset);
+
+  // Enable the Master/Slave Mode
+  TIM_SelectMasterSlaveMode(TIM2, TIM_MasterSlaveMode_Enable);
+
+  // enable interrupt
+  TIM_ITConfig(TIM2, TIM_IT_CC1, ENABLE);
+
+  // TIM enable counter
+  TIM_Cmd(TIM2, ENABLE);
+
+  // enable global interrupt
+  NVIC_InitTypeDef NVIC_InitStructure;
+  NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = MIOS32_IRQ_PRIO_MID;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+  return 0; // no error
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Should be called each millisecond to check for capture events
+// Return value:
+// - FRQ_METER_NO_UPDATE: no update since last call
+// - FRQ_METER_TOO_LOW: if frequency too low
+// - FRQ_METER_TOO_HIGH: if frequency too high
+// - otherwise: the frequency * 1000
+/////////////////////////////////////////////////////////////////////////////
+u32 FRQ_METER_Tick(void)
+{
+  // check for timeout
+  if( ++timeout_ctr >= 30*NUM_CAP_SAMPLES ) { // sufficient for 30 Hz
+    timeout_ctr = 0;
+    frq_meter_current_frq = FRQ_METER_TOO_LOW;
+    return FRQ_METER_TOO_LOW;
+  } else if( !frq_updated ) {
+    return FRQ_METER_NO_UPDATE;
+  }
+
+  MIOS32_IRQ_Disable();
+  u32 frq = frq_meter_current_frq;
+  frq_updated = 0;
+  MIOS32_IRQ_Enable();
+
+  return frq;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//! Interrupt handler for TIM2
+/////////////////////////////////////////////////////////////////////////////
+void TIM2_IRQHandler(void)
+{
+  if( TIM_GetITStatus(TIM2, TIM_IT_CC1) != RESET ) {
+    TIM_ClearITPendingBit(TIM2, TIM_IT_CC1);
+
+    // clear timeout
+    timeout_ctr = 0;
+
+    // get the Input Capture value
+    u16 cap1 = TIM_GetCapture1(TIM2); // this will clear the CC1 flag
+    //u16 cap2 = TIM_GetCapture2(TIM2); // this will clear the CC2 flag
+
+    // overcaptured? (means: register not read early enough, will happen at frequencies > 1 kHz)
+    // SR should be read after Capture1 read
+    if( TIM2->SR & TIM_FLAG_CC1OF ) {
+      TIM2->SR = ~TIM_FLAG_CC1OF;
+      cap_sample_ctr = 0;
+      frq_meter_current_frq = FRQ_METER_TOO_HIGH;
+      frq_updated = 1;
+    } else if( TIM2->SR & TIM_FLAG_CC4 ) {
+      // timer overrun!
+      TIM2->SR = ~TIM_FLAG_CC4;
+      cap_sample_ctr = 0;
+      frq_meter_current_frq = FRQ_METER_TOO_LOW;
+      frq_updated = 1;
+    } else {
+      // store capture value
+      cap_samples[cap_sample_ctr++] = cap1;
+
+      if( cap_sample_ctr >= NUM_CAP_SAMPLES ) {
+	cap_sample_ctr = 0;
+
+	u32 sum = 0;
+	int i;
+	for(i=0; i<NUM_CAP_SAMPLES; ++i)
+	  sum += cap_samples[i];
+	u32 mean = sum / NUM_CAP_SAMPLES;
+
+	// ooof! 2.25 MHz * 1000 still in 32bit range
+	frq_meter_current_frq = 2250000000U / mean;
+	frq_updated = 1;
+      }
+    }
+  }
+}
