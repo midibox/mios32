eece0dc70f90df3e2fe59a53805cdeb24430a667
tk <tk@midibox.org>
2011-04-21 21:18:25 +0000

    SID module: support for MBNet transfers

--- modules/sid/sid.c
+++ modules/sid/sid.c
@@ -20,12 +20,15 @@
 #include "sid.h"
 //#include "sidemu.h"
 
+#if SID_USE_MBNET
+#include <mbnet.h>
+#endif
 
 /////////////////////////////////////////////////////////////////////////////
 // Help Macros
 /////////////////////////////////////////////////////////////////////////////
 
-#ifdef MIOS32_FAMILY_STM32F10x
+#if !SID_USE_MBNET && defined(MIOS32_FAMILY_STM32F10x)
 # define PIN_SER(b)  { SID_SER_PORT->BSRR = (b) ? SID_SER_PIN : (SID_SER_PIN << 16); }
 
 # define PIN_RCLK_0  { SID_RCLK_PORT->BRR  = SID_RCLK_PIN; }
@@ -33,6 +36,24 @@
 
 # define PIN_SCLK_0  { SID_SCLK_PORT->BRR  = SID_SCLK_PIN; }
 # define PIN_SCLK_1  { SID_SCLK_PORT->BSRR = SID_SCLK_PIN; }
+#endif
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Local Definitions
+/////////////////////////////////////////////////////////////////////////////
+
+#if SID_USE_MBNET
+#define MBNET_TX_STATE_NOP   0
+#define MBNET_TX_STATE_SID1  1
+#define MBNET_TX_STATE_SID2  2
+#define MBNET_TX_STATE_SID3  3
+#define MBNET_TX_STATE_SID4  4
+#define MBNET_TX_STATE_SID5  5
+#define MBNET_TX_STATE_SID6  6
+#define MBNET_TX_STATE_SID7  7
+#define MBNET_TX_STATE_SID8  8
+#define MBNET_TX_STATE_DONE  9
 #endif
 
 
@@ -48,6 +69,9 @@ sid_regs_t sid_regs[SID_NUM];
 /////////////////////////////////////////////////////////////////////////////
 
 static sid_regs_t sid_regs_shadow[SID_NUM]; // to determine register changes
+#if SID_USE_MBNET
+static u32 sid_regs_shadow_updated[SID_NUM];
+#endif
 
 static const u8 update_order[SID_REGS_NUM] = { 
    0,  1,  2,  3,  5,  6, // voice 1 w/o osc control register
@@ -58,9 +82,18 @@ static const u8 update_order[SID_REGS_NUM] = {
   25, 26, 27, 28, 29, 30, 31 // SwinSID registers
 };
 
+static u8 sid_available;
 
+#if SID_USE_MBNET
+static u8 mbnet_tx_state;
+static u8 mbnet_tx_reg_ctr;
+static u8 mbnet_my_node_id;
+static u32 mbnet_tx_msg_ctr;
+static u32 mbnet_tx_msg_ctr_min;
+static u32 mbnet_tx_msg_ctr_max;
+#endif
 
-#ifdef MIOS32_FAMILY_STM32F10x
+#if !SID_USE_MBNET && defined(MIOS32_FAMILY_STM32F10x)
 typedef struct {
   GPIO_TypeDef *port;
   u16 pin_mask;
@@ -102,8 +135,11 @@ typedef struct {
 // Local Prototypes
 /////////////////////////////////////////////////////////////////////////////
 
+#if !SID_USE_MBNET
 static inline void SID_UpdateReg(sid_cs_pin_t *cs_pin0, sid_cs_pin_t *cs_pin1, u8 cs, u8 addr, u8 data, u8 reset);
-
+#else
+s32 SID_MBNET_TxHandler(mbnet_id_t *mbnet_id, mbnet_msg_t *msg, u8 *dlc);
+#endif
 
 /////////////////////////////////////////////////////////////////////////////
 // Initializes the SID module
@@ -118,12 +154,28 @@ s32 SID_Init(u32 mode)
   if( mode != 0 )
     return -1; // unsupported mode
 
+  // register available SIDs (can be changed during runtime)
+#if SID_NUM > 8
+# error "SID_NUM > 8 not allowed"
+#endif
+
+#if SID_USE_MBNET
+  sid_available = 0x00; // set after node scan
+  mbnet_tx_state = MBNET_TX_STATE_NOP;
+  mbnet_tx_reg_ctr = 0;
+  mbnet_tx_msg_ctr = 0;
+  mbnet_tx_msg_ctr_min = 0;
+  mbnet_tx_msg_ctr_max = 0;
+  mbnet_my_node_id = 0xff;
+#else
+  sid_available = (u8)((1 << SID_NUM)-1);
+#endif
+
 #ifdef SIDEMU_ENABLED
   SYNTH_Init(0);
 #endif
 
-#ifndef SIDPHYS_DISABLED
-#ifdef MIOS32_FAMILY_STM32F10x
+#if !SID_USE_MBNET && defined(MIOS32_FAMILY_STM32F10x) && !defined(SIDPHYS_DISABLED)
   // GPIO initialisation
   GPIO_InitTypeDef GPIO_InitStructure;
   GPIO_StructInit(&GPIO_InitStructure);
@@ -207,19 +259,57 @@ s32 SID_Init(u32 mode)
   TIM_ARRPreloadConfig(TIM4, ENABLE);
   TIM_Cmd(TIM4, ENABLE);
 #endif
-#endif
 
   // clear all SID registers
-  for(sid=0; sid<SID_NUM; ++sid)
+  for(sid=0; sid<SID_NUM; ++sid) {
     for(reg=0; reg<SID_REGS_NUM; ++reg) {
       sid_regs[sid].ALL[reg] = 0;
       sid_regs_shadow[sid].ALL[reg] = 0;
     }
+#if SID_USE_MBNET
+    sid_regs_shadow_updated[sid] = 0xffffffff;
+#endif
+  }
 
   // force reset and transfer all values to SID registers
   SID_Update(2);
 
   return 0; // no error
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Access to "SID Available" flags
+// Each of up to 8 SIDs has a flag - if set to 0, it won't be updated
+/////////////////////////////////////////////////////////////////////////////
+s32 SID_AvailableSet(u8 available)
+{
+  sid_available = available;
+
+  MIOS32_IRQ_Disable();
+  if( sid_available && mbnet_tx_state == MBNET_TX_STATE_NOP ) {
+    // start MBNet transfers once SIDs have been found
+    mbnet_tx_state = MBNET_TX_STATE_SID1;
+    mbnet_tx_reg_ctr = 0;
+    mbnet_tx_msg_ctr = 0;
+    mbnet_tx_msg_ctr_min = 0;
+    mbnet_tx_msg_ctr_max = 0;
+    mbnet_my_node_id = MBNET_NodeIDGet();
+    MBNET_InstallTxHandler(SID_MBNET_TxHandler);
+  } else if( !sid_available && mbnet_tx_state != MBNET_TX_STATE_NOP ) {
+    // stop MBNet transfers
+    mbnet_tx_state = MBNET_TX_STATE_NOP;
+    MBNET_InstallTxHandler(NULL);
+  }
+  MIOS32_IRQ_Enable();
+
+  return 0; // no error
+}
+
+u8 SID_AvailableGet(void)
+{
+  return sid_available;
 }
 
 
@@ -230,6 +320,127 @@ s32 SID_Init(u32 mode)
 //             if 2: reset SID, thereafter force transfer of all registers
 // OUT: returns < 0 if update failed
 /////////////////////////////////////////////////////////////////////////////
+#if SID_USE_MBNET
+s32 SID_Update(u32 mode)
+{
+  int sid, reg;
+  // if register update should be forced, just inverse all shadow register values
+  if( mode >= 1 ) { // (also for reset mode)
+    MIOS32_IRQ_Disable();
+    for(sid=0; sid<SID_NUM; ++sid)
+      sid_regs_shadow_updated[sid] = 0xffffffff;
+    MIOS32_IRQ_Enable();
+  }
+
+  // transfer SID registers to shadow registers and check for updates
+  MIOS32_IRQ_Disable();
+  for(sid=0; sid<SID_NUM; ++sid) {
+    u8 *regs = (u8 *)&sid_regs[sid];
+    u8 *regs_shadow = (u8 *)&sid_regs_shadow[sid];
+
+    for(reg=0; reg<SID_REGS_NUM; ++reg) {
+      if( *regs_shadow != *regs )
+	sid_regs_shadow_updated[sid] |= (1 << reg);
+      *regs_shadow++ = *regs++;
+    }
+  }
+  MIOS32_IRQ_Enable();
+
+  // trigger next update
+  if( mbnet_tx_state == MBNET_TX_STATE_DONE && sid_available ) {
+    if( mbnet_tx_msg_ctr && (!mbnet_tx_msg_ctr_min || mbnet_tx_msg_ctr < mbnet_tx_msg_ctr_min) )
+      mbnet_tx_msg_ctr_min = mbnet_tx_msg_ctr;
+    if( mbnet_tx_msg_ctr > mbnet_tx_msg_ctr_max )
+      mbnet_tx_msg_ctr_max = mbnet_tx_msg_ctr;
+
+    mbnet_tx_msg_ctr = 0;
+    mbnet_tx_reg_ctr = 0;
+
+    mbnet_tx_state = MBNET_TX_STATE_SID1;
+    MBNET_TriggerTxHandler();
+  }
+
+  return 0; // no error
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Transfer handler for SID register changes
+// Periodically called in background whenever a new message can be sent
+// Returns 1 if a new message is available
+/////////////////////////////////////////////////////////////////////////////
+s32 SID_MBNET_TxHandler(mbnet_id_t *mbnet_id, mbnet_msg_t *msg, u8 *dlc)
+{
+  if( mbnet_tx_state == MBNET_TX_STATE_DONE )
+    return 0; // nothing else to do...
+
+  // - search for next register set of 8 bytes which have been updated (-> tx_required)
+  // - check if there are remaining bytes which have to be updated - if not, set remote_reg_update
+  // - if all registers for all SIDs have been updated, change to MBNET_TX_STATE_DONE
+  u8 tx_sid = 0;
+  u8 tx_addr = 0;
+  u8 tx_required = 0;
+  u8 remote_reg_update = 0;
+  while( 1 ) {
+    u8 sid = (mbnet_tx_state-MBNET_TX_STATE_SID1);
+    u8 addr = mbnet_tx_reg_ctr;
+
+    if( sid_regs_shadow_updated[sid] & (0xff << addr) ) {
+      if( tx_required )
+	break; // ok, this isn't the last update for this SID
+      else {
+	tx_required = 1;
+	tx_sid = sid;
+	tx_addr = addr;
+      }
+    }
+
+    mbnet_tx_reg_ctr += 8;
+    if( mbnet_tx_reg_ctr >= SID_REGS_NUM ) {
+      mbnet_tx_reg_ctr = 0;
+      if( ++mbnet_tx_state == MBNET_TX_STATE_SID3 ) {
+	mbnet_tx_state = MBNET_TX_STATE_DONE;
+
+	if( !tx_required )
+	  return 0; // abort loop, because register update has finished
+	else {
+	  remote_reg_update = 1; // update SID registers at remote side
+	  break;
+	}
+      }
+    }
+  }
+
+  // create MBNet message
+  mbnet_id->control = (remote_reg_update ? 0xfd00 : 0xfe00) + tx_addr + 0x20*(tx_sid&1);
+  mbnet_id->tos     = MBNET_REQ_RAM_WRITE;
+  mbnet_id->ms      = mbnet_my_node_id >> 4;
+  mbnet_id->ack     = 0;
+  mbnet_id->node    = 0x00 + (tx_sid/2);
+
+#if 0
+  DEBUG_MSG("%02x: %04x\n", mbnet_id->node, mbnet_id->control);
+#endif
+
+  *dlc = 8;
+
+  MIOS32_IRQ_Disable();
+  u8 *regs_shadow = (u8 *)&sid_regs_shadow[tx_sid].ALL[tx_addr];
+  msg->data_l  = *regs_shadow++;
+  msg->data_l |= (u32)*regs_shadow++ << 8;
+  msg->data_l |= (u32)*regs_shadow++ << 16;
+  msg->data_l |= (u32)*regs_shadow++ << 24;
+  msg->data_h  = *regs_shadow++;
+  msg->data_h |= (u32)*regs_shadow++ << 8;
+  msg->data_h |= (u32)*regs_shadow++ << 16;
+  msg->data_h |= (u32)*regs_shadow++ << 24;
+  sid_regs_shadow_updated[tx_sid] &= ~(0xff << tx_addr);
+  MIOS32_IRQ_Enable();
+
+  ++mbnet_tx_msg_ctr;
+
+  return 1;
+}
+#else
 s32 SID_Update(u32 mode)
 {
   int sid, reg, i;
@@ -237,7 +448,7 @@ s32 SID_Update(u32 mode)
   // trigger reset?
   if( mode == 2 ) {
     // (cs pins don't need to be set during reset, but the SID_UpdateReg() routine requires the pointers)
-#ifdef MIOS32_FAMILY_STM32F10x
+#if defined(MIOS32_FAMILY_STM32F10x)
     sid_cs_pin_t *cs_pin0 = (sid_cs_pin_t *)&sid_cs_pin[0];
     sid_cs_pin_t *cs_pin1 = (sid_cs_pin_t *)&sid_cs_pin[1];
 #else
@@ -265,7 +476,7 @@ s32 SID_Update(u32 mode)
     u8 cs_both = (3 << (2*sid));
     u8 cs_l_only = (1 << (2*sid));
     u8 cs_r_only = (2 << (2*sid));
-#ifdef MIOS32_FAMILY_STM32F10x
+#if defined(MIOS32_FAMILY_STM32F10x)
     sid_cs_pin_t *cs_pin0 = (sid_cs_pin_t *)&sid_cs_pin[2*sid+0];
     sid_cs_pin_t *cs_pin1 = (sid_cs_pin_t *)&sid_cs_pin[2*sid+1];
 #else
@@ -421,5 +632,24 @@ static inline void SID_UpdateReg(sid_cs_pin_t *cs_pin0, sid_cs_pin_t *cs_pin1, u
   MIOS32_IRQ_Enable();
 #endif
 #endif
+}
+#endif
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Can be called periodically (e.g. each second) to output statistics
+/////////////////////////////////////////////////////////////////////////////
+s32 SID_PrintStatistics(void)
+{
+#if SID_USE_MBNET
+  if( mbnet_tx_msg_ctr_min ) {
+    MIOS32_MIDI_SendDebugMessage("MBNET MSG Min:%d Max:%d", mbnet_tx_msg_ctr_min, mbnet_tx_msg_ctr_max);
+
+    mbnet_tx_msg_ctr_min = 0;
+    mbnet_tx_msg_ctr_max = 0;
+  }
+#endif
+
+  return 0; // no error
 }
 
