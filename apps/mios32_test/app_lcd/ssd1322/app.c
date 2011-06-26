6b2abdcd675074c15ecf8e261f0a192eef84b312
hawkeye <peter@knoblach.de>
2011-06-26 11:40:23 +0000

    Added voxel space graphics demo (ssd1322 controller for Newhaven 256x64 OLEDs)

--- apps/mios32_test/app_lcd/ssd1322/app.c
+++ apps/mios32_test/app_lcd/ssd1322/app.c
@@ -7,7 +7,7 @@
  *  Copyright (C) 2008 Thorsten Klose (tk@midibox.org)
  *  Licensed for personal non-commercial use only.
  *  All other rights reserved.
- * 
+ *
  * ==========================================================================
  */
 
@@ -31,154 +31,241 @@ void APP_Init(void)
 }
 
 
+
+// -------------------------------------------------------------------------------------------
+// Voxelspace demo by Hawkeye
+
+u8 field[128][128];             // Heightfield
+u8 screen[64][64];              // Screen buffer [y][x]
+
+
+// Limits a value to 0...255 
+//
+int limit(int x)
+{
+  return x < 0 ? 0 : (x > 255 ? 255 : x);
+}
+// -------------------------------------------------------------------------------------------
+
+
+// Landscape/heightfield generator
+//
+void calcField(void)
+{
+  int p, i, j, k, k2, p2;
+
+  field[0][0]=128;
+  
+  // "Cloud" landscape generator
+  for (p = 256; p > 1; p = p2)
+  {
+     p2 = p >> 1;
+     k = p * 8 + 20;
+     k2 = k >> 1;
+
+     for (i = 0; i < 128; i += p)
+     {
+        for (j = 0; j < 128; j += p)
+        {
+           int a = field[i][j];
+           int b = field[(i + p) & 127][j];
+           int c = field[i][(j + p) & 127];
+           int d = field[(i + p) & 127][(j + p) & 127];
+
+           field[i][(j + p2) & 127] = limit(((a + c)>>1)+(rand() % k - k2));
+           field[(i + p2) & 127][(j + p2) & 127]= limit(((a + b + c + d)>>2)+(rand() % k - k2));
+           field[(i + p2) & 127][j]= limit(((a + b) >> 1)+(rand() % k - k2));
+        }
+     }
+  }
+
+  // Blur
+  for (k=0; k<1; k++)
+     for (i=1; i<127; i++)
+        for (j=1; j<127; j++)
+        {
+           u16 four = field[i-1][j] + field[i+1][j] + field[i][j-1] + field[i][j+1];
+           four = four >> 2;
+           field[i][j] = (field[i][j] + four) >> 1;
+         }
+}
+// -------------------------------------------------------------------------------------------
+
+
+// Voxel space display demo
+// 
+// Note: this is not very optimized yet, especially some float multiplications/divisions
+// can be improved... When this is done, the x-resolution could also be increased,
+// by casting more rays... it is currently only using 64 pixels horizontal resolution.
+// but it works for now... greets, hawkeye.
+//
+void runVoxel(void)
+{
+   u16 y; // position
+   u16 i, j, k;
+
+   calcField();
+
+   // initial clear screen   
+   for (j = 0; j < 64; j++)
+      for (i = 0; i < 64; i++)
+         screen[j][i] = j < 6 ? ((j << 4) + j) : 0;
+
+   // endless animation, if it needs to be stopped, poll here
+   while (1)
+   {
+      for (y = 30000; y >=1 ; y--)
+      {
+         for (j=0; j<30; j++)  // Distance from screen plane
+         {
+            u16 distscaler = 38 -j ;
+            u16 stdheight = 400 / distscaler;
+            u16 endy = (float) stdheight*1.5;
+            if (endy > 63) endy = 63;
+
+            u16 yfield = j + y;
+
+            yfield = yfield % 255;
+            if (yfield > 127)
+               yfield = 255-yfield;
+
+            for (i = 0; i < 64; i++)
+            {
+               u16 xfield = (64 + (int)((i-32) * (float)distscaler/10)) & 127;
+               u8 h = field[yfield][xfield];
+
+               u16 height_val= (float)h/distscaler; // Scale height to distance
+
+               u16 starty = stdheight - height_val;
+               
+               starty = (float)starty*1.5;
+               if (starty < 0) starty = 0;
+
+               u8 col = (h & 0xf0) + (h >> 4);
+               for (k = starty; k < endy; k++)
+                  screen[k][i] = col;
+            }
+         }
+
+         for (j = 0; j < 64; j++)
+         {
+            APP_LCD_Cmd(0x15);
+            APP_LCD_Data(0+0x1c);
+
+            APP_LCD_Cmd(0x75);
+            APP_LCD_Data(j);
+
+            APP_LCD_Cmd(0x5c);
+
+            for (i = 0; i < 64; i++)
+            {
+               APP_LCD_Data(screen[j][i]);
+               APP_LCD_Data(screen[j][i]);
+               screen[j][i] = j < 6 ? ((j << 4) + j) : 0;
+            }
+         }
+      }
+   }
+}
+// ----------------------------------------------------------------------------------------
+
+
+// Render test screen, one half is "full on" for flicker tests
+// one half contains a color gradient pattern
+//
+void testScreen()
+{
+  u16 x = 0;
+  u16 y = 0;
+
+  for (y = 0; y < 64; y++)
+  {
+     APP_LCD_Cmd(0x15);
+     APP_LCD_Data(0x1c);
+
+     APP_LCD_Cmd(0x75);
+     APP_LCD_Data(y);
+
+     APP_LCD_Cmd(0x5c);
+
+     for (x = 0; x < 64; x++)
+     {
+        if (x < 32)
+        {  // half screen pattern
+        
+           if (x || 4 == 0 || y || 4 == 0)
+           {
+              APP_LCD_Data(y & 0x0f);
+              APP_LCD_Data(0);
+           }
+           else
+           {
+              APP_LCD_Data(0x00);
+              APP_LCD_Data(0x00);
+           }
+        }
+        else // half screen "white"
+        {
+           APP_LCD_Data(0xff);
+           APP_LCD_Data(0xff);
+        }
+     }
+  }
+  
+  while(1);
+}
+// -------------------------------------------------------------------------------------------
+
+
 /////////////////////////////////////////////////////////////////////////////
 // This task is running endless in background
 /////////////////////////////////////////////////////////////////////////////
 void APP_Background(void)
 {
-  // print static screen
-  MIOS32_LCD_FontInit((u8 *)GLCD_FONT_NORMAL);
+  runVoxel();
 
-  // clear LCD
-  MIOS32_LCD_Clear();
-
-#if 0
-  while( 1 );
-#endif
-
-  // print text
-  MIOS32_LCD_GCursorSet(4, 2*8);
-  MIOS32_LCD_PrintString("          SSD1322 horizontal            ");
-
-  MIOS32_LCD_GCursorSet(4, 3*8);
-  MIOS32_LCD_PrintString("     ---> best performance!!! <---      ");
-
-  MIOS32_LCD_GCursorSet(30, 5*8);
-  MIOS32_LCD_PrintString(" powered by  ");
-
-  MIOS32_LCD_FontInit((u8 *)GLCD_FONT_BIG);
-  MIOS32_LCD_GCursorSet(110, 4*8);
-  MIOS32_LCD_PrintString("MIOS32");
-
-  // endless loop: print animations
-  u32 loop_ctr = 0;
-  u8 dir = 1;
-  u8 knob_icon_ctr[4] = {0, 3, 6, 9}; // memo: 12 icons
-  u8 knob_icon_delay_ctr[4] = {0, 2, 4, 6};
-  const u8 knob_icon_x[4] = {0, 212, 0, 212}; // memo: icon width 28
-  const u8 knob_icon_y[4] = {0, 0, 40, 40}; // memo: icon height 24
-
-  u8 vmeter_icon_ctr[2] = {0, 5}; // memo: 28 icons (14 used)
-  u8 vmeter_icon_dir[2] = {1, 1};
-  u8 vmeter_icon_delay_ctr[2] = {1, 4};
-  const u8 vmeter_icon_x[2] = {28, 204}; // memo: icon width 8
-  const u8 vmeter_icon_y[2] = {16, 16}; // memo: icon height 32
-
-  u8 hmeter_icon_ctr[2] = {6, 11}; // memo: 28 icons (14 used)
-  u8 hmeter_icon_dir[2] = {1, 0};
-  u8 hmeter_icon_delay_ctr[2] = {4, 2};
-  const u8 hmeter_icon_x[2] = {106, 106}; // memo: icon width 28
-  const u8 hmeter_icon_y[2] = {0, 56}; // memo: icon height 8
-
-  while( 1 ) {
-    s32 i;
-
-    // toggle the state of all LEDs (allows to measure the execution speed with a scope)
-    MIOS32_BOARD_LED_Set(0xffffffff, ~MIOS32_BOARD_LED_Get());
-
-    // switch direction each 10000 cycles
-    if( dir ) {
-      if( ++loop_ctr > 10000 ) {
-	loop_ctr = 0;
-	dir = 0;
-      }
-    } else {
-      if( ++loop_ctr > 10000 ) {
-	loop_ctr = 0;
-	dir = 1;
-      }
-    }
-
-    // print turning Knob icons at all edges
-    MIOS32_LCD_FontInit((u8 *)GLCD_FONT_KNOB_ICONS); // memo: 12 icons, icon size: 28x24
-    for(i=0; i<4; ++i) {
-      if( ++knob_icon_delay_ctr[i] > 10 ) {
-	knob_icon_delay_ctr[i] = 0;
-	if( ++knob_icon_ctr[i] >= 12 )
-	  knob_icon_ctr[i] = 0;
-      }
-      MIOS32_LCD_GCursorSet(knob_icon_x[i], knob_icon_y[i]);
-      MIOS32_LCD_PrintChar(knob_icon_ctr[i]);
-    }
-
-    // print vmeter icons
-    MIOS32_LCD_FontInit((u8 *)GLCD_FONT_METER_ICONS_V); // memo: 28 icons, 14 used, icon size: 8x32
-    for(i=0; i<2; ++i) {
-      if( ++vmeter_icon_delay_ctr[i] > 5 ) {
-	vmeter_icon_delay_ctr[i] = 0;
-	if( vmeter_icon_dir[i] ) {
-	  if( ++vmeter_icon_ctr[i] >= 13 )
-	    vmeter_icon_dir[i] = 0;
-	} else {
-	  if( --vmeter_icon_ctr[i] < 1 )
-	    vmeter_icon_dir[i] = 1;
-	}
-      }
-      MIOS32_LCD_GCursorSet(vmeter_icon_x[i], vmeter_icon_y[i]);
-      MIOS32_LCD_PrintChar(vmeter_icon_ctr[i]);
-    }
-
-    // print hmeter icons
-    MIOS32_LCD_FontInit((u8 *)GLCD_FONT_METER_ICONS_H); // memo: 28 icons, 14 used, icon size: 28x8
-    for(i=0; i<2; ++i) {
-      if( ++hmeter_icon_delay_ctr[i] > 7 ) {
-	hmeter_icon_delay_ctr[i] = 0;
-	if( hmeter_icon_dir[i] ) {
-	  if( ++hmeter_icon_ctr[i] >= 13 )
-	    hmeter_icon_dir[i] = 0;
-	} else {
-	  if( --hmeter_icon_ctr[i] < 1 )
-	    hmeter_icon_dir[i] = 1;
-	}
-      }
-      MIOS32_LCD_GCursorSet(hmeter_icon_x[i], hmeter_icon_y[i]);
-      MIOS32_LCD_PrintChar(hmeter_icon_ctr[i]);
-    }
-  }
+  // Alternative: "Test screen for parameter tuning"
+  // testScreen();
 }
 
 
 /////////////////////////////////////////////////////////////////////////////
 // This hook is called when a MIDI package has been received
+// Very useful for tuning display initialization parameters
+// Note - only tune when no other activity is going on (parallel data writes)
+// good when using static "testScreen()"
 /////////////////////////////////////////////////////////////////////////////
 void APP_MIDI_NotifyPackage(mios32_midi_port_t port, mios32_midi_package_t midi_package)
 {
-  // for testing
+  // For testing
   if( midi_package.type == CC ) {
     switch( midi_package.cc_number ) {
     case 0x10:
       APP_LCD_Cmd(0xb3); // Set_Display_Clock(0x91);
-      APP_LCD_Data(midi_package.value);
+      APP_LCD_Data((u8)midi_package.value/2);
       break;
     case 0x11:
-      APP_LCD_Cmd(0xca); // Set_Multiplex_Ratio(0x3F);
-      APP_LCD_Data(midi_package.value);
-      break;
+       APP_LCD_Cmd(0xca); // Set_Multiplex_Ratio(0x3F);
+       APP_LCD_Data((u8)midi_package.value/2);
     case 0x12:
       APP_LCD_Cmd(0xb1); // Set_Phase_Length(0xE2);
-      APP_LCD_Data(midi_package.value);
+      APP_LCD_Data((u8)midi_package.value/2);
       break;
     case 0x13:
       APP_LCD_Cmd(0xbb); // Set_Precharge_Voltage(0x1F);
-      APP_LCD_Data(midi_package.value);
+      APP_LCD_Data((u8)midi_package.value*2);
       break;
     case 0x14:
       APP_LCD_Cmd(0xb6); // Set_Precharge_Period(0x08);
-      APP_LCD_Data(midi_package.value);
+      APP_LCD_Data((u8)midi_package.value*2);
       break;
     case 0x15:
       APP_LCD_Cmd(0xbe); // Set_VCOMH(0x07);
-      APP_LCD_Data(midi_package.value);
+      APP_LCD_Data((u8)midi_package.value*2);
+      break;
+    case 0x16:
+       APP_LCD_Cmd(0xc1); // Constrast Current
+       APP_LCD_Data(midi_package.value*2);
       break;
     }
   }
@@ -226,3 +313,5 @@ void APP_ENC_NotifyChange(u32 encoder, s32 incrementer)
 void APP_AIN_NotifyChange(u32 pin, u32 pin_value)
 {
 }
+
+
