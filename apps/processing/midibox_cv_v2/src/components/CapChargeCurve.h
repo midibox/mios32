958c57cdfe347444248ab364123ed3e908bbb6d1
tk <tk@midibox.org>
2012-03-19 22:43:47 +0000

    MBCV: added missing CapChargeCurve

---
+++ apps/processing/midibox_cv_v2/src/components/CapChargeCurve.h
@@ -0,0 +1,22 @@
+/* -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*- */
+// $Id$
+/*
+ * Capacitor Charge Curve
+ *
+ * ==========================================================================
+ *
+ *  Copyright (C) 2012 Thorsten Klose (tk@midibox.org)
+ *  Licensed for personal non-commercial use only.
+ *  All other rights reserved.
+ * 
+ * ==========================================================================
+ */
+
+#ifndef _CAP_CHARGE_CURVE_H
+#define _CAP_CHARGE_CURVE_H
+
+#define CAP_CHARGE_CURVE_STEPS 1024
+
+extern u16 capChargeCurve[CAP_CHARGE_CURVE_STEPS];
+
+#endif /* _CAP_CHARGE_CURVE_H */
