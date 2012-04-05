826476e4ce32599e44b36e26922046b97cf54eb6
tk <tk@midibox.org>
2012-04-05 13:04:37 +0000

    MBSEQ Remix page: skip remix pattern correctly

--- apps/sequencers/midibox_seq_v4/mios32/mios32_config.h
+++ apps/sequencers/midibox_seq_v4/mios32/mios32_config.h
@@ -14,7 +14,7 @@
 // The boot message which is print during startup and returned on a SysEx query
 #define MIOS32_LCD_BOOT_MSG_DELAY 0 // we delay the boot and print a message inside the app
 //                                <---------------------->
-#define MIOS32_LCD_BOOT_MSG_LINE1 "MIDIbox SEQ V4.057"
+#define MIOS32_LCD_BOOT_MSG_LINE1 "MIDIbox SEQ V4.058"
 #define MIOS32_LCD_BOOT_MSG_LINE2 "(C) 2012 T. Klose"
 
 // USB settings
