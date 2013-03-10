a6369c20e16dc504ca109737153289487f6c26f4
tk <tk@midibox.org>
2013-03-10 16:30:57 +0000

    MIDIO128: improved MIDI file loop synchronisation

--- apps/controllers/midio128_v3/src/mios32_config.h
+++ apps/controllers/midio128_v3/src/mios32_config.h
@@ -12,7 +12,7 @@
 
 // The boot message which is print during startup and returned on a SysEx query
 #define MIOS32_LCD_BOOT_MSG_DELAY 0 // we delay the boot and print a message inside the app
-#define MIOS32_LCD_BOOT_MSG_LINE1 "MIDIO 128 V3.013"
+#define MIOS32_LCD_BOOT_MSG_LINE1 "MIDIO 128 V3.014"
 #define MIOS32_LCD_BOOT_MSG_LINE2 "(C) 2012 T.Klose"
 
 // define a unique VID/PID for this application
