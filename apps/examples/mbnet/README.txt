639e3a214f7017308a189be587bc37bce4c0300c
tk <tk@midibox.org>
2013-09-15 16:50:28 +0000

    MBNET: changed LPC17 pin config for MBHP_CORE_LPC17 and added demo app

---
+++ apps/examples/mbnet/README.txt
@@ -0,0 +1,23 @@
+$Id$
+
+MIDIbox SID OSC->MBNet Proxy
+===============================================================================
+Copyright (C) 2013 Thorsten Klose (tk@midibox.org)
+Licensed for personal non-commercial use only.
+All other rights reserved.
+===============================================================================
+
+Required tools:
+  -> http://svnmios.midibox.org/filedetails.php?repname=svn.mios32&path=%2Ftrunk%2Fdoc%2FMEMO
+
+===============================================================================
+
+Required hardware:
+   o 2 x MBHP_CORE_STM32 or MBHP_CORE_LPC17, connected together via J18
+
+===============================================================================
+
+For background informations see:
+http://midibox.org/forums/topic/18245-mbnet-issues-custom-build
+
+===============================================================================
--- modules/mbnet/mbnet.c
+++ modules/mbnet/mbnet.c
@@ -19,6 +19,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 #include <mios32.h>
+#include <string.h>
 
 #include <FreeRTOS.h>
 #include <portmacro.h>
@@ -32,7 +33,6 @@
 /////////////////////////////////////////////////////////////////////////////
 // for optional debugging messages via MIDI
 /////////////////////////////////////////////////////////////////////////////
-#define DEBUG_VERBOSE_LEVEL 1
 #define DEBUG_MSG MIOS32_MIDI_SendDebugMessage
 
 
@@ -72,10 +72,9 @@ static u8          last_req_dlc;
 // turns to 1 if scan for MBNet nodes is finished
 static u8 scan_finished;
 
-#if DEBUG_VERBOSE_LEVEL >= 2
-// only for debugging: skip messages after more than 8 TOS=1/2
+// for debugging
+static u8 verbose_level = 1; // default verbose level
 static u32 tos12_ctr = 0;
-#endif
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -107,17 +106,35 @@ s32 MBNET_Init(u32 mode)
 
   // init hardware
   if( MBNET_HAL_Init(mode) < 0 ) {
-#if DEBUG_VERBOSE_LEVEL >= 1
-    DEBUG_MSG("[MBNET] ----- initialisation FAILED! ------------------------------------------\n");
-#endif
+    if( verbose_level >= 1 ) {
+      DEBUG_MSG("[MBNET] ----- initialisation FAILED! ------------------------------------------\n");
+    }
     return -1;
   }
 
-#if DEBUG_VERBOSE_LEVEL >= 2
-  DEBUG_MSG("[MBNET] ----- initialized -----------------------------------------------------\n");
-#endif
+    if( verbose_level >= 2 ) {
+      DEBUG_MSG("[MBNET] ----- initialized -----------------------------------------------------\n");
+    }
 
   return 0; // no error
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Sets the verbose level (should be 1 by default, can be up to 3)
+/////////////////////////////////////////////////////////////////////////////
+s32 MBNET_VerboseLevelSet(u8 level)
+{
+  if( level <= 3 ) {
+    verbose_level = level;
+    return 0; // no error
+  }
+  return -1; // invalid verbose level
+}
+
+s32 MBNET_VerboseLevelGet()
+{
+  return verbose_level;
 }
 
 
@@ -259,17 +276,17 @@ static s32 MBNET_SendMsg(mbnet_id_t mbnet_id, mbnet_msg_t msg, u8 dlc)
   if( (status=MBNET_HAL_Send(mbnet_id, msg, dlc)) < 0 )
     return status;
 
-#if DEBUG_VERBOSE_LEVEL >= 3
-  if( tos12_ctr <= 8 ) {
-    DEBUG_MSG("[MBNET] sent %s: ID:%02x TOS=%d DLC=%d MSG=%02x %02x %02x %02x %02x %02x %02x %02x\n",
-	      mbnet_id.ack ? "ACK" : "REQ",
-	      mbnet_id.node,
-	      mbnet_id.tos,
-	      dlc,
-	      msg.bytes[0], msg.bytes[1], msg.bytes[2], msg.bytes[3],
-	      msg.bytes[4], msg.bytes[5], msg.bytes[6], msg.bytes[7]);
-  }
-#endif
+    if( verbose_level >= 3 ) {
+      if( tos12_ctr <= 8 ) {
+	DEBUG_MSG("[MBNET] sent %s: ID:0x%02x TOS=%d DLC=%d MSG=%02x %02x %02x %02x %02x %02x %02x %02x\n",
+		  mbnet_id.ack ? "ACK" : "REQ",
+		  mbnet_id.node,
+		  mbnet_id.tos,
+		  dlc,
+		  msg.bytes[0], msg.bytes[1], msg.bytes[2], msg.bytes[3],
+		  msg.bytes[4], msg.bytes[5], msg.bytes[6], msg.bytes[7]);
+      }
+    }
 
   return status;
 }
@@ -407,32 +424,32 @@ s32 MBNET_WaitAck_NonBlocking(u8 slave_id, mbnet_msg_t *ack_msg, u8 *dlc)
 
       // response from expected slave? if not - ignore it!
       if( (p.id.control & 0xff) == slave_id ) {
-#if DEBUG_VERBOSE_LEVEL >= 3
-	DEBUG_MSG("[MBNET] got ACK from slave ID %02x TOS=%d DLC=%d MSG=%02x %02x %02x %02x %02x %02x %02x %02x\n",
-		  p.id.control & 0xff,
-		  p.id.tos,
-		  *dlc,
-		  ack_msg->bytes[0], ack_msg->bytes[1], ack_msg->bytes[2], ack_msg->bytes[3],
-		  ack_msg->bytes[4], ack_msg->bytes[5], ack_msg->bytes[6], ack_msg->bytes[7]);
-#endif
+	if( verbose_level >= 3 ) {
+	  DEBUG_MSG("[MBNET] got ACK from slave ID 0x%02x TOS=%d DLC=%d MSG=%02x %02x %02x %02x %02x %02x %02x %02x\n",
+		    p.id.control & 0xff,
+		    p.id.tos,
+		    *dlc,
+		    ack_msg->bytes[0], ack_msg->bytes[1], ack_msg->bytes[2], ack_msg->bytes[3],
+		    ack_msg->bytes[4], ack_msg->bytes[5], ack_msg->bytes[6], ack_msg->bytes[7]);
+	}
 
 	// retry requested?
 	if( p.id.tos == MBNET_ACK_RETRY ) {
-#if DEBUG_VERBOSE_LEVEL >= 3
-	  DEBUG_MSG("[MBNET] Slave ID %02x requested to retry the transfer!\n", slave_id);
-#endif
+	  if( verbose_level >= 3 ) {
+	    DEBUG_MSG("[MBNET] Slave ID 0x%02x requested to retry the transfer!\n", slave_id);
+	  }
 	  return -4; // slave requested to retry
 	}
 
 	return 0; // wait ack successful!
       } else {
-#if DEBUG_VERBOSE_LEVEL >= 1
-	DEBUG_MSG("[MBNET] ERROR: ACK from unexpected slave ID %02x (TOS=%d DLC=%d MSG=%02x %02x %02x...)\n",
-		  p.id.control & 0xff,
-		  p.id.tos,
-		  *dlc,
-		  ack_msg->bytes[0], ack_msg->bytes[1], ack_msg->bytes[2]);
-#endif
+	  if( verbose_level >= 3 ) {
+	    DEBUG_MSG("[MBNET] ERROR: ACK from unexpected slave ID 0x%02x (TOS=%d DLC=%d MSG=%02x %02x %02x...)\n",
+		      p.id.control & 0xff,
+		      p.id.tos,
+		      *dlc,
+		      ack_msg->bytes[0], ack_msg->bytes[1], ack_msg->bytes[2]);
+	  }
       }
     }
   } while( got_msg );
@@ -468,9 +485,9 @@ s32 MBNET_WaitAck(u8 slave_id, mbnet_msg_t *ack_msg, u8 *dlc)
   } while( (error == -4 || error == -5) && ++timeout_ctr < MBNET_TIMEOUT_CTR_MAX );
 
   if( timeout_ctr == MBNET_TIMEOUT_CTR_MAX ) {
-#if DEBUG_VERBOSE_LEVEL >= 3
-    DEBUG_MSG("[MBNET] ACK polling for slave ID %02x timed out!\n", slave_id);
-#endif
+    if( verbose_level >= 3 ) {
+      DEBUG_MSG("[MBNET] ACK polling for slave ID 0x%02x timed out!\n", slave_id);
+    }
     return -6; // timeout
   }
 
@@ -550,17 +567,17 @@ s32 MBNET_Handler(void (*callback)(u8 master_id, mbnet_tos_req_t tos, u16 contro
 	    slave_nodes_ix[ix_ix] = 0xff; // disable retry counter
 	  }
 
-#if DEBUG_VERBOSE_LEVEL >= 2
-	  DEBUG_MSG("[MBNET] new slave found: ID %02x (Ix=%d) P:%d T:%c%c%c%c V:%d.%d\n", 
-		    search_slave_id,
-		    slave_nodes_ix[ix_ix],
-		    ack_msg.protocol_version,
-		    ack_msg.node_type[0], ack_msg.node_type[1], ack_msg.node_type[2], ack_msg.node_type[3],
-		    ack_msg.node_version, ack_msg.node_subversion);
+	  if( verbose_level >= 2 ) {
+	    DEBUG_MSG("[MBNET] new slave found: ID 0x%02x (Ix=%d) P:%d T:%c%c%c%c V:%d.%d\n", 
+		      search_slave_id,
+		      slave_nodes_ix[ix_ix],
+		      ack_msg.protocol_version,
+		      ack_msg.node_type[0], ack_msg.node_type[1], ack_msg.node_type[2], ack_msg.node_type[3],
+		      ack_msg.node_version, ack_msg.node_subversion);
 
-	  if( slave_nodes_ix[ix_ix] == 0xff )
-	    DEBUG_MSG("[MBNET] unfortunately no free info slot anymore!\n");
-#endif
+	    if( slave_nodes_ix[ix_ix] == 0xff )
+	      DEBUG_MSG("[MBNET] unfortunately no free info slot anymore!\n");
+	  }
 	}
       }
     }
@@ -574,9 +591,9 @@ s32 MBNET_Handler(void (*callback)(u8 master_id, mbnet_tos_req_t tos, u16 contro
 	  finished = 0;
 
       if( finished && !scan_finished ) {
-#if DEBUG_VERBOSE_LEVEL >= 2
-	DEBUG_MSG("[MBNET] Scan finished.\n");
-#endif
+	if( verbose_level >= 2 ) {
+	  DEBUG_MSG("[MBNET] Scan finished.\n");
+	}
 	scan_finished = 1;
       }
     }
@@ -585,9 +602,7 @@ s32 MBNET_Handler(void (*callback)(u8 master_id, mbnet_tos_req_t tos, u16 contro
   u8 got_msg;
   mbnet_packet_t p;
   s8 locked = -1; // contains MS node ID if slave has been locked, otherwise -1 if not locked
-#if DEBUG_VERBOSE_LEVEL >= 2
   tos12_ctr = 0; // only for debugging
-#endif
   do {
     got_msg = MBNET_HAL_ReceiveReq(&p);
 
@@ -595,27 +610,29 @@ s32 MBNET_Handler(void (*callback)(u8 master_id, mbnet_tos_req_t tos, u16 contro
       // master node ID
       u8 master_id = p.id.ms << 4;
 
-#if DEBUG_VERBOSE_LEVEL >= 2
       if( p.id.tos == 1 || p.id.tos == 2 ) {
 	++tos12_ctr;
 	if( tos12_ctr > 8 ) {
-	  DEBUG_MSG("[MBNET] ... skip display of remaining read/write operations\n");
+	  if( verbose_level >= 2 ) {
+	    DEBUG_MSG("[MBNET] ... skip display of remaining read/write operations\n");
+	  }
 	}
       } else {
 	tos12_ctr = 0;
       }
-#endif
-#if DEBUG_VERBOSE_LEVEL >= 3
-      if( tos12_ctr <= 8 ) {
-	DEBUG_MSG("[MBNET] request ID=%02x TOS=%d CTRL=%04x DLC=%d MSG=%02x %02x %02x %02x %02x %02x %02x %02x\n",
-		  master_id,
-		  p.id.tos,
-		  p.id.control,
-		  p.dlc,
-		  p.msg.bytes[0], p.msg.bytes[1], p.msg.bytes[2], p.msg.bytes[3],
-		  p.msg.bytes[4], p.msg.bytes[5], p.msg.bytes[6], p.msg.bytes[7]);
+
+      if( verbose_level >= 3 ) {
+	if( tos12_ctr <= 8 ) {
+	  DEBUG_MSG("[MBNET] request ID=0x%02x TOS=%d CTRL=0x%04x DLC=%d MSG=%02x %02x %02x %02x %02x %02x %02x %02x\n",
+		    master_id,
+		    p.id.tos,
+		    p.id.control,
+		    p.dlc,
+		    p.msg.bytes[0], p.msg.bytes[1], p.msg.bytes[2], p.msg.bytes[3],
+		    p.msg.bytes[4], p.msg.bytes[5], p.msg.bytes[6], p.msg.bytes[7]);
+	}
       }
-#endif
+
       // default acknowledge message is empty
       mbnet_msg_t ack_msg;
       ack_msg.data_l = ack_msg.data_h = 0;
@@ -623,9 +640,9 @@ s32 MBNET_Handler(void (*callback)(u8 master_id, mbnet_tos_req_t tos, u16 contro
       // if slave has been locked by another master: send retry acknowledge
       if( locked != -1 && p.id.ms != locked ) {
 	MBNET_SendAck(master_id, MBNET_ACK_RETRY, ack_msg, 0); // master_id, tos, msg, dlc
-#if DEBUG_VERBOSE_LEVEL >= 3
-	DEBUG_MSG("[MBNET] ignore - node is locked by MS=%d\n", locked);
-#endif
+	if( verbose_level >= 3 ) {
+	  DEBUG_MSG("[MBNET] ignore - node is locked by MS=%d\n", locked);
+	}
       } else {
 	// branch depending on TOS
 	switch( p.id.tos ) {
@@ -757,9 +774,9 @@ static s32 MBNET_BusErrorCheck(void)
 
   // TODO: try to recover
 
-#if DEBUG_VERBOSE_LEVEL >= 1
-  DEBUG_MSG("[MBNET] ERRORs detected - permanent off state reached!\n");
-#endif
+  if( verbose_level >= 1 ) {
+    DEBUG_MSG("[MBNET] ERRORs detected - permanent off state reached!\n");
+  }
 
   // recovery not possible: prevent MBNet accesses
   mbnet_state.PERMANENT_OFF = 1;
@@ -787,4 +804,166 @@ s32 MBNET_InstallTxHandler(s32 (*tx_handler_callback)(mbnet_id_t *mbnet_id, mbne
 s32 MBNET_TriggerTxHandler(void)
 {
   return MBNET_HAL_TriggerTxHandler();
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// help function which parses a decimal or hex value
+// returns >= 0 if value is valid
+// returns -1 if value is invalid
+/////////////////////////////////////////////////////////////////////////////
+static s32 get_dec(char *word)
+{
+  if( word == NULL )
+    return -1;
+
+  char *next;
+  long l = strtol(word, &next, 0);
+
+  if( word == next )
+    return -1;
+
+  return l; // value is valid
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Returns help page for implemented terminal commands of this module
+/////////////////////////////////////////////////////////////////////////////
+s32 MBNET_TerminalHelp(void *_output_function)
+{
+  void (*out)(char *format, ...) = _output_function;
+
+  out("  mbnet:                            prints status informations");
+  out("  mbnet_reconnect:                  (re-)scans for MBNET nodes on the bus");
+  out("  set mbnet_id <0x00..0x7f>:        changes my MBNET ID (current ID: 0x%02x)\n", MBNET_NodeIDGet());
+  out("  set mbnet_verbose <0..4>:         enables MBNET debug messages (verbose level: %d)\n", MBNET_VerboseLevelGet());
+
+  return 0; // no error
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Parser for a complete line
+// Returns > 0 if command line matches with UIP terminal commands
+/////////////////////////////////////////////////////////////////////////////
+s32 MBNET_TerminalParseLine(char *input, void *_output_function)
+{
+  void (*out)(char *format, ...) = _output_function;
+  char *separators = " \t";
+  char *brkt;
+  char *parameter;
+
+  // since strtok_r works destructive (separators in *input replaced by NUL), we have to restore them
+  // on an unsuccessful call (whenever this function returns < 1)
+  int input_len = strlen(input);
+
+  if( (parameter = strtok_r(input, separators, &brkt)) ) {
+    
+    if( strcasecmp(parameter, "mbnet") == 0 ) {
+      MBNET_TerminalPrintStatus(_output_function);
+      return 1; // command taken
+    } else if( strcasecmp(parameter, "mbnet_reconnect") == 0 ) {
+      MBNET_Reconnect();
+      out("Scanning for MBNET nodes...");
+      return 1; // command taken
+    } else if( strcasecmp(parameter, "set") == 0 ) {
+      if( !(parameter = strtok_r(NULL, separators, &brkt)) ) {
+	out("Missing parameter after 'set'!");
+	return 1; // command taken
+      }
+
+      if( strcasecmp(parameter, "mbnet_id") == 0 ) {
+	s32 value = -1;
+	if( (parameter = strtok_r(NULL, separators, &brkt)) )
+	  value = get_dec(parameter);
+
+	if( value < 0x00 || value > 0x7f ) {
+	  out("Expecting value between 0x00..0x7f!");
+	  return 1; // command taken
+	}
+
+	if( MBNET_NodeIDSet(value) >= 0 ) {
+	  out("MBNET ID changed to 0x%02x.", MBNET_NodeIDGet());
+	} else {
+	  out("Failed to change ID!");
+	}
+	return 1; // command taken
+
+      } else if( strcasecmp(parameter, "mbnet_verbose") == 0 ) {
+	s32 value = -1;
+	if( (parameter = strtok_r(NULL, separators, &brkt)) )
+	  value = get_dec(parameter);
+
+	if( value < 0 || value > 3 ) {
+	  out("Expecting value between 0..3!");
+	  return 1; // command taken
+	}
+
+	if( MBNET_VerboseLevelSet(value) >= 0 ) {
+	  out("MBNET Verbose Level set to %d.", MBNET_VerboseLevelGet());
+	} else {
+	  out("Failed to change verbose level!");
+	}
+	return 1; // command taken
+
+      } else {
+	// out("Unknown command - type 'help' to list available commands!");
+      }
+    }
+  }
+
+  // restore input line (replace NUL characters by spaces)
+  int i;
+  char *input_ptr = input;
+  for(i=0; i<input_len; ++i, ++input_ptr)
+    if( !*input_ptr )
+      *input_ptr = ' ';
+
+  return 0; // command not taken
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Keyboard Configuration (can also be called from external)
+/////////////////////////////////////////////////////////////////////////////
+s32 MBNET_TerminalPrintStatus(void *_output_function)
+{
+  void (*out)(char *format, ...) = _output_function;
+
+  out("My MBNET ID: 0x%02x", MBNET_NodeIDGet());
+
+  if( mbnet_state.PERMANENT_OFF ) {
+    out("MBNET State: permanent off!");
+  } else if( mbnet_state.PANIC ) {
+    out("MBNET State: panic!");
+  } else {
+    out("MBNET State: running");
+  }
+
+  out("MBNET Scan State: %s", MBNET_ScanFinished() ? "finished" : "in progress");
+
+  {
+    u8 num_ix = MBNET_SLAVE_NODES_END-MBNET_SLAVE_NODES_BEGIN+1;
+    u8 ix;
+    for(ix=0; ix<num_ix; ++ix) {
+      u8 slave_id = ix + MBNET_SLAVE_NODES_BEGIN;
+      if( slave_nodes_ix[ix] >= 128 ) {
+	out("Slave #%2d: not found", ix+1);
+      } else {
+	mbnet_msg_t *info;
+	if( MBNET_SlaveNodeInfoGet(slave_id, &info) >= 0 ) {
+	  out("Slave #%2d  ID 0x%02x  P:%d T:%c%c%c%c V:%d.%d\n", 
+	      ix,
+	      slave_id,
+	      info->protocol_version,
+	      info->node_type[0], info->node_type[1], info->node_type[2], info->node_type[3],
+	      info->node_version, info->node_subversion);
+	}
+      }
+    }
+  }
+
+  out("MBNET Verbose Level: %d", MBNET_VerboseLevelGet());
+
+  return 0; // no error
 }
