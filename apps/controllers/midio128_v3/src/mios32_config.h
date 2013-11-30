e5cc1ac3b177b148d83e35ffbeff2954ce19ce1b
tk <tk@midibox.org>
2013-11-30 20:19:31 +0000

    MIDIO128: corrected MIDI clock output

--- apps/controllers/midio128_v3/src/mios32_config.h
+++ apps/controllers/midio128_v3/src/mios32_config.h
@@ -12,7 +12,7 @@
 
 // The boot message which is print during startup and returned on a SysEx query
 #define MIOS32_LCD_BOOT_MSG_DELAY 0 // we delay the boot and print a message inside the app
-#define MIOS32_LCD_BOOT_MSG_LINE1 "MIDIO 128 V3.016"
+#define MIOS32_LCD_BOOT_MSG_LINE1 "MIDIO 128 V3.017"
 #define MIOS32_LCD_BOOT_MSG_LINE2 "(C) 2013 T.Klose"
 
 // define a unique VID/PID for this application
