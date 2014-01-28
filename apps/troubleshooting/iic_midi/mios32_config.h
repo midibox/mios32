bd774098e0668fa48e08cf33fd6016b99aa2b53b
tk <tk@midibox.org>
2014-01-28 22:48:01 +0000

    iic_midi_v1_001

--- apps/troubleshooting/iic_midi/mios32_config.h
+++ apps/troubleshooting/iic_midi/mios32_config.h
@@ -11,8 +11,8 @@
 #define _MIOS32_CONFIG_H
 
 // The boot message which is print during startup and returned on a SysEx query
-#define MIOS32_LCD_BOOT_MSG_LINE1 "IIC_MIDI Test"
-#define MIOS32_LCD_BOOT_MSG_LINE2 "(C) 2013 T.Klose"
+#define MIOS32_LCD_BOOT_MSG_LINE1 "IIC_MIDI Test V1.001"
+#define MIOS32_LCD_BOOT_MSG_LINE2 "(C) 2014 T.Klose"
 
 
 // IIC port J4A (=0) or J4B (=1)?
