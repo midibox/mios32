4810abee09db35fb9af5a6588215b1afd9a70d94
tk <tk@midibox.org>
2014-03-07 19:15:50 +0000

    MBNG: improved AIN Switch mode

--- apps/controllers/midibox_ng_v1/src/mbng_ain.h
+++ apps/controllers/midibox_ng_v1/src/mbng_ain.h
@@ -33,7 +33,7 @@
 extern s32 MBNG_AIN_Init(u32 mode);
 
 extern s32 MBNG_AIN_HandleCalibration(u16 pin_value, u16 min, u16 max, u16 ain_max, u8 spread_center);
-extern s32 MBNG_AIN_HandleAinMode(mbng_event_item_t *item, u16 value, u16 prev_value, s16 min, s16 max);
+extern s32 MBNG_AIN_HandleAinMode(mbng_event_item_t *item, u16 pin_value, u16 prev_pin_value);
 
 extern s32 MBNG_AIN_NotifyChange(u32 pin, u32 pin_value, u8 no_midi);
 extern s32 MBNG_AIN_NotifyReceivedValue(mbng_event_item_t *item);
