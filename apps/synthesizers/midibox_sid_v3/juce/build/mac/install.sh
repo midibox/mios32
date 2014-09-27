c4d94437de161d11c74e16a97a6b6dfbd402c2e0
tk <tk@midibox.org>
2014-09-27 23:41:37 +0000

    MBSID: removed obsolete files

--- apps/synthesizers/midibox_sid_v3/juce/build/mac/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-/bin/rm -rf ~/Library/Audio/Plug-Ins/Components/MIDIboxSID.component
-/bin/cp -r build/Debug/MIDIboxSID.component ~/Library/Audio/Plug-Ins/Components
-auval -v aumu msid MBTK
