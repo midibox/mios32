2aa9a597b5ca028623f5fd4c440f773adbe0b2e4
tk <tk@midibox.org>
2015-05-24 19:04:58 +0000

    goom synth: added copyright info

--- apps/synthesizers/goom/src/synth.h
+++ apps/synthesizers/goom/src/synth.h
@@ -1,6 +1,23 @@
 // $Id$
 /*
  * ==========================================================================
+ * Copyright 2014 Mark Owen
+ * http://www.quinapalus.com
+ * E-mail: goom@quinapalus.com
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License
+ * as published by the Free Software Foundation.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this file.  If not, see <http://www.gnu.org/licenses/> or
+ * write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
  * ==========================================================================
  */
 
