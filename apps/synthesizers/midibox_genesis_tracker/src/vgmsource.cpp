820a120e6045ea673a166efb47ee9937b6f1dc7f
Sauraen <sauraen@gmail.com>
2016-01-24 17:03:57 +0000

    Linking not working

--- apps/synthesizers/midibox_genesis_tracker/src/vgmsource.cpp
+++ apps/synthesizers/midibox_genesis_tracker/src/vgmsource.cpp
@@ -13,6 +13,18 @@
 #include <mios32.h>
 #include "vgmsource.h"
 
+VgmSource::VgmSource(){
+    vgmdatastartaddr = 0;
+    opn2clock = 8000000;
+    psgclock = 4000000;
+    loopaddr = 0;
+    loopsamples = 0;
+}
+
+VgmSource::~VgmSource(){
+    
+}
+
 void VgmSource::readHeader(){
     if(getByte(0) == 'V' && getByte(1) == 'g' && getByte(2) == 'm' && getByte(3) == ' '){
         //File has header
@@ -21,8 +33,8 @@ void VgmSource::readHeader(){
         u8 ver_hi = getByte(9);
         psgclock = ((u32)getByte(0x0C)) | ((u32)getByte(0x0D) << 8)
                  | ((u32)getByte(0x0E) << 16) | ((u32)getByte(0x0F) << 24);
-        loopaddr = ((u32)getByte(0x1C)) | ((u32)getByte(0x1D) << 8)
-                 | ((u32)getByte(0x1E) << 16) | ((u32)getByte(0x1F) << 24)
+        loopaddr = (((u32)getByte(0x1C)) | ((u32)getByte(0x1D) << 8)
+                 | ((u32)getByte(0x1E) << 16) | ((u32)getByte(0x1F) << 24))
                  + 0x1C;
         loopsamples = ((u32)getByte(0x20)) | ((u32)getByte(0x21) << 8)
                  | ((u32)getByte(0x22) << 16) | ((u32)getByte(0x23) << 24);
