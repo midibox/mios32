cb3cc1eb71e215f8e240ed914426996ec9b9d7ba
Sauraen <sauraen@gmail.com>
2016-02-14 21:22:53 +0000

    Cleaned up SVN

---
+++ apps/synthesizers/midibox_genesis_tracker/src/genesisstate.h
@@ -0,0 +1,27 @@
+/*
+ * MIDIbox Quad Genesis: Genesis State Drawing Functions
+ *
+ * ==========================================================================
+ *
+ *  Copyright (C) 2016 Sauraen (sauraen@gmail.com)
+ *  Licensed for personal non-commercial use only.
+ *  All other rights reserved.
+ * 
+ * ==========================================================================
+ */
+
+#ifndef _GENESISSTATE_H
+#define _GENESISSTATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void DrawGenesisActivity(u8 g);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* _GENESISSTATE_H */
