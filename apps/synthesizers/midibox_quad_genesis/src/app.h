7e20b8119b16781db4b3c76b68c9226a15f914d5
Sauraen <sauraen@gmail.com>
2016-04-21 03:04:51 +0000

    Beginning of real MIDIbox Quad Genesis project

---
+++ apps/synthesizers/midibox_quad_genesis/src/app.h
@@ -0,0 +1,62 @@
+/*
+ * MIDIbox Quad Genesis: Main application header
+ *
+ * ==========================================================================
+ *
+ *  Copyright (C) 2016 Sauraen (sauraen@gmail.com)
+ *  Licensed for personal non-commercial use only.
+ *  All other rights reserved.
+ * 
+ * ==========================================================================
+ */
+
+#ifndef _APP_H
+#define _APP_H
+
+
+#include "vgm.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// Global definitions
+/////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Global Types
+/////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Prototypes
+/////////////////////////////////////////////////////////////////////////////
+
+extern void APP_Init(void);
+extern void APP_Background(void);
+extern void APP_Tick(void);
+extern void APP_MIDI_Tick(void);
+extern void APP_MIDI_NotifyPackage(mios32_midi_port_t port, mios32_midi_package_t midi_package);
+extern void APP_SRIO_ServicePrepare(void);
+extern void APP_SRIO_ServiceFinish(void);
+extern void APP_DIN_NotifyToggle(u32 pin, u32 pin_value);
+extern void APP_ENC_NotifyChange(u32 encoder, s32 incrementer);
+extern void APP_AIN_NotifyChange(u32 pin, u32 pin_value);
+
+/////////////////////////////////////////////////////////////////////////////
+// Export global variables
+/////////////////////////////////////////////////////////////////////////////
+
+#define MAXNUMFILES 200
+extern char* filenamelist;
+extern s32 numfiles;
+extern u8 selfile;
+extern u8 updatescreen;
+extern u8 selgenesis;
+extern u8 vegasactive;
+extern u8 selgvoice;
+extern VgmSource* sources[4];
+extern VgmHead* heads[4];
+extern VgmSource* qsource;
+extern VgmHead* qhead;
+
+
+#endif /* _APP_H */
