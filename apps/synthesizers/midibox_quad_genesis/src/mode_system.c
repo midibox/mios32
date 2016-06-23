e6a2d519825823e35bcd72a1911f239d9f286410
Sauraen <sauraen@gmail.com>
2016-06-23 02:03:38 +0000

    Synth engine in progress

--- apps/synthesizers/midibox_quad_genesis/src/mode_system.c
+++ apps/synthesizers/midibox_quad_genesis/src/mode_system.c
@@ -13,6 +13,10 @@
 #include <mios32.h>
 #include "mode_system.h"
 #include <vgm.h>
+#include <genesis.h>
+#include "frontpanel.h"
+
+u8 submode;
 
 void DrawUsage(){
     vgm_meminfo_t meminfo = VGM_PerfMon_GetMemInfo();
@@ -22,12 +26,35 @@ void DrawUsage(){
     MIOS32_LCD_CursorSet(0,0);
     MIOS32_LCD_PrintFormattedString("RAM %d/%d Chip %d%% Card %d%%", meminfo.numusedblocks, meminfo.numblocks, chipuse, carduse);
 }
+void DrawMenu(){
+    switch(submode){
+        case 0:
+            DrawUsage();
+            MIOS32_LCD_CursorSet(0,1);
+            MIOS32_LCD_PrintFormattedString("Fltr");
+            break;
+        case 1:
+            MIOS32_LCD_Clear();
+            MIOS32_LCD_CursorSet(0,0);
+            MIOS32_LCD_PrintFormattedString("3kHz 6dB LPF (MD1 sound): FM   PSG");
+            MIOS32_LCD_CursorSet(26,1);
+            MIOS32_LCD_PrintFormattedString("%s  %s", 
+                    (genesis[0].board.cap_opn2 ? "On " : "Off"), 
+                    (genesis[0].board.cap_psg  ? "On " : "Off"));
+            break;
+        default:
+            MIOS32_LCD_Clear();
+            MIOS32_LCD_CursorSet(0,0);
+            MIOS32_LCD_PrintFormattedString("System invalid submode %d!", submode);
+    }
+}
 
 void Mode_System_Init(){
-    
+    submode = 0;
 }
 void Mode_System_GotFocus(){
-    DrawUsage();
+    submode = 0;
+    DrawMenu();
 }
 
 void Mode_System_Tick(){
@@ -45,7 +72,37 @@ void Mode_System_BtnGVoice(u8 gvoice, u8 state){
 
 }
 void Mode_System_BtnSoftkey(u8 softkey, u8 state){
-
+    if(!state) return;
+    switch(submode){
+        case 0:
+            switch(softkey){
+                case 0:
+                    submode = 1;
+                    DrawMenu();
+                    break;
+            }
+            break;
+        case 1:
+            switch(softkey){
+                case 5:
+                    genesis[0].board.cap_opn2 = !genesis[0].board.cap_opn2;
+                    Genesis_WriteBoardBits(0);
+                    MIOS32_LCD_CursorSet(26,1);
+                    MIOS32_LCD_PrintFormattedString(genesis[0].board.cap_opn2 ? "On " : "Off");
+                    break;
+                case 6:
+                    genesis[0].board.cap_psg = !genesis[0].board.cap_psg;
+                    Genesis_WriteBoardBits(0);
+                    MIOS32_LCD_CursorSet(31,1);
+                    MIOS32_LCD_PrintFormattedString(genesis[0].board.cap_psg ? "On " : "Off");
+                    break;
+            }
+            break;
+        default:
+            MIOS32_LCD_Clear();
+            MIOS32_LCD_CursorSet(0,0);
+            MIOS32_LCD_PrintFormattedString("System invalid submode %d!", submode);
+    }
 }
 void Mode_System_BtnSelOp(u8 op, u8 state){
 
@@ -54,7 +111,11 @@ void Mode_System_BtnOpMute(u8 op, u8 state){
 
 }
 void Mode_System_BtnSystem(u8 button, u8 state){
-
+    if(!state) return;
+    if(button == FP_B_MENU){
+        submode = 0;
+        DrawMenu();
+    }
 }
 void Mode_System_BtnEdit(u8 button, u8 state){
 
---
+++ apps/synthesizers/midibox_quad_genesis/src/syeng.h
@@ -0,0 +1,118 @@
+/*
+ * MIDIbox Quad Genesis: Synth engine header
+ *
+ * ==========================================================================
+ *
+ *  Copyright (C) 2016 Sauraen (sauraen@gmail.com)
+ *  Licensed for personal non-commercial use only.
+ *  All other rights reserved.
+ * 
+ * ==========================================================================
+ */
+
+#ifndef _SYENG_H
+#define _SYENG_H
+
+#include <genesis.h>
+#include <vgm.h>
+
+typedef union {
+    u32 all;
+    struct {
+        //Don't change the order of these bits, they're accessed by bit mask operations
+        u8 fm1:1;
+        u8 fm2:1;
+        u8 fm3:1;
+        u8 fm4:1;
+        u8 fm5:1;
+        u8 fm6:1;
+        
+        u8 fm1_lfo:1;
+        u8 fm2_lfo:1;
+        u8 fm3_lfo:1;
+        u8 fm4_lfo:1;
+        u8 fm5_lfo:1;
+        u8 fm6_lfo:1;
+        
+        u8 dac:1;
+        u8 fm3_special:1;
+        u8 opn2_globals:1;
+        u8 lfofixed:1;
+        
+        u8 lfofixedspeed:3;
+        u8 dummy:5;
+        
+        u8 sq1:1;
+        u8 sq2:1;
+        u8 sq3:1;
+        u8 noise:1;
+        u8 noisefreqsq3:1;
+        u8 dummy2:3;
+    };
+} usage_t;
+
+
+
+typedef struct {
+    usage_t usage;
+    
+} syngenesis_t;
+
+extern syngenesis_t syngenesis[GENESIS_COUNT];
+
+typedef struct {
+    usage_t usage;
+    VgmSource* initsource;
+    VgmSource* noteonsource;
+    VgmSource* noteoffsource;
+    u8 rootnote;
+    u8 dummy1;
+    u16 dummy2;
+} synprogram_t;
+
+
+typedef struct {
+    u8 valid:1;
+    u8 playing:1;
+    u8 playinginit:1;
+    u8 sourcechannel;
+    u8 note;
+    u8 dummy2;
+    u32 recency;
+    VgmHead_Channel mapping[12];
+    VgmHead* head;
+} synproginstance_t;
+
+#ifndef MBQG_NUM_PROGINSTANCES
+#define MBQG_NUM_PROGINSTANCES 10*GENESIS_COUNT
+#endif
+
+extern synproginstance_t proginstances[MBQG_NUM_PROGINSTANCES];
+
+#if (MBQG_NUM_PROGINSTANCES > 64)
+#error "Too many proginstances (6 bit index)!"
+#endif
+
+typedef struct {
+    u8 trackermode:1;
+    u8 trackervoice:7;
+    u8 dummy1;
+    u16 dummy2;
+    synprogram_t* program;
+} synchannel_t;
+
+#ifndef MBQG_NUM_PORTS
+#define MBQG_NUM_PORTS 4
+#endif
+
+extern synchannel_t channels[16*MBQG_NUM_PORTS];
+
+////////////////////////////////////////////////////////////////////////////////
+
+extern void SyEng_Init();
+extern void SyEng_Tick();
+
+extern void SyEng_Note_On(mios32_midi_package_t pkg);
+extern void SyEng_Note_Off(mios32_midi_package_t pkg);
+
+#endif /* _SYENG_H */
