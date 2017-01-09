c06f31592d179e98170de17a0f2d2c1d5570a2db
tk <tk@midibox.org>
2017-01-09 20:57:54 +0000

    midibox_seq_v4_092

--- apps/sequencers/midibox_seq_v4/mios32/mios32_config.h
+++ apps/sequencers/midibox_seq_v4/mios32/mios32_config.h
@@ -15,7 +15,7 @@
 #define MIOS32_LCD_BOOT_MSG_DELAY 0 // we delay the boot and print a message inside the app
 //                                <---------------------->
 #define MIOS32_LCD_BOOT_MSG_LINE1 "MIDIbox SEQ V4.092"
-#define MIOS32_LCD_BOOT_MSG_LINE2 "(C) 2016 T. Klose"
+#define MIOS32_LCD_BOOT_MSG_LINE2 "(C) 2017 T. Klose"
 
 // USB settings
 #define MIOS32_USB_PRODUCT_STR  "MIDIbox SEQ V4"
