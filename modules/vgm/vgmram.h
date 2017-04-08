05ac7386084674aaec32fa470385d61b1411e868
Sauraen <sauraen@gmail.com>
2017-04-08 23:31:40 +0000

    Preparations for VGM saving

--- modules/vgm/vgmram.h
+++ modules/vgm/vgmram.h
@@ -36,11 +36,11 @@ extern void VGM_HeadRAM_Delete(void* headram);
 extern void VGM_HeadRAM_Restart(VgmHead* head);
 extern void VGM_HeadRAM_cmdNext(VgmHead* head, u32 vgm_time);
 
-extern void VGM_HeadRAM_Forward1(VgmHead* head);
-extern void VGM_HeadRAM_Backward1(VgmHead* head);
-extern void VGM_HeadRAM_SeekTo(VgmHead* head, u32 newaddr);
-extern void VGM_HeadRAM_ForwardState(VgmHead* head, u32 maxt, u32 maxdt, u8 allowstay);
-extern void VGM_HeadRAM_BackwardState(VgmHead* head, u32 maxt, u32 maxdt);
+extern s32 VGM_HeadRAM_Forward1(VgmHead* head);
+extern s32 VGM_HeadRAM_Backward1(VgmHead* head);
+extern s32 VGM_HeadRAM_SeekTo(VgmHead* head, u32 newaddr);
+extern s32 VGM_HeadRAM_ForwardState(VgmHead* head, u32 maxt, u32 maxdt, u8 allowstay);
+extern s32 VGM_HeadRAM_BackwardState(VgmHead* head, u32 maxt, u32 maxdt);
 
 typedef union {
     u8 ALL[8];
