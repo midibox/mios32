c33f43fedc638e4233103d822b0ff08cc026262f
tk <tk@midibox.org>
2020-01-27 21:22:13 +0100

    MBSEQ: fixed hanging gates issue

--- apps/sequencers/midibox_seq_v4/mios32/mios32_config.h
+++ apps/sequencers/midibox_seq_v4/mios32/mios32_config.h
@@ -20,9 +20,9 @@
 #define MIOS32_LCD_BOOT_MSG_DELAY 0 // we delay the boot and print a message inside the app
 //                                <---------------------->
 #ifdef MBSEQV4P
-#define MIOS32_LCD_BOOT_MSG_LINE1 "MIDIbox SEQ V4+.096"
+#define MIOS32_LCD_BOOT_MSG_LINE1 "MIDIbox SEQ V4+.097"
 #else
-#define MIOS32_LCD_BOOT_MSG_LINE1 "MIDIbox SEQ V4.096"
+#define MIOS32_LCD_BOOT_MSG_LINE1 "MIDIbox SEQ V4.097"
 #endif
 #define MIOS32_LCD_BOOT_MSG_LINE2 "(C) 2020 T. Klose"
 
