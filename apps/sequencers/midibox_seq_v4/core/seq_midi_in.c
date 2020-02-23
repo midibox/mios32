0ee2ba9c6b234b7b64f0371d304b9ac9d1570030
tk <tk@midibox.org>
2020-02-23 17:44:30 +0100

    MBSEQ: fixed rec channel selection & group selection

--- apps/sequencers/midibox_seq_v4/core/seq_midi_in.c
+++ apps/sequencers/midibox_seq_v4/core/seq_midi_in.c
@@ -510,6 +510,7 @@ s32 SEQ_MIDI_IN_Receive(mios32_midi_port_t port, mios32_midi_package_t midi_pack
           u8 track = midi_package.chn;
           if( track != SEQ_UI_VisibleTrackGet() ) {
             SEQ_RECORD_Enable(0, 1);
+            ui_selected_group = track >> 2;
             ui_selected_tracks = (1 << track);
             seq_record_state.ARMED_TRACKS = ui_selected_tracks;
             seq_ui_display_update_req = 1;
