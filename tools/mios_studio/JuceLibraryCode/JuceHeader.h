c2ad66f3b66b827eb27b8eefc934da2f28161243
tk <tk@midibox.org>
2023-10-21 20:27:23 +0200

    MIOS Studio updates for MacOS

--- tools/mios_studio/JuceLibraryCode/JuceHeader.h
+++ tools/mios_studio/JuceLibraryCode/JuceHeader.h
@@ -48,7 +48,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "MIOS_Studio";
     const char* const  companyName    = "MIDIbox";
-    const char* const  versionString  = "2.4.8";
-    const int          versionNumber  = 0x20408;
+    const char* const  versionString  = "2.4.9";
+    const int          versionNumber  = 0x20409;
 }
 #endif
